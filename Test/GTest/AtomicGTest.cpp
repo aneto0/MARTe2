33c229f6c278d0a3ea229dd5235905eba60d6481
Giuseppe Ferrò <frgpp01@uniroma2.it>
2015-06-25 08:12:07 +0200

    Added template to AtomicGTest.cpp #126

--- Test/GTest/AtomicGTest.cpp
+++ Test/GTest/AtomicGTest.cpp
@@ -1,11 +1,44 @@
-/*
- *  AtomicGTest.cpp
+/**
+ * @file AtomicGTest.cpp
+ * @brief Source file for class AtomicGTest
+ * @date 25/giu/2015
+ * @author Giuseppe Ferrò
  *
- *  Created on: Feb 5, 2015
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This source file contains the definition of all the methods for
+ * the class AtomicGTest (public, protected, and private). Be aware that some 
+ * methods, such as those inline could be defined on the header file, instead.
  */
+
+/*---------------------------------------------------------------------------*/
+/*                         Standard header includes                          */
+/*---------------------------------------------------------------------------*/
 #include <limits.h>
+
+/*---------------------------------------------------------------------------*/
+/*                         Project header includes                           */
+/*---------------------------------------------------------------------------*/
+
 #include "gtest/gtest.h"
 #include "AtomicTest.h"
+/*---------------------------------------------------------------------------*/
+/*                           Static definitions                              */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                           Method definitions                              */
+/*---------------------------------------------------------------------------*/
 
 class AtomicGTest: public ::testing::Test {
 protected:
@@ -22,36 +55,37 @@ protected:
 
 TEST_F(AtomicGTest,Int32Test) {
     int32 testValue = 32;
-    AtomicTest< int32 > int32Test(testValue);
+    AtomicTest<int32> int32Test(testValue);
     ASSERT_TRUE(int32Test.All());
 }
 
 TEST_F(AtomicGTest,Int16Test) {
     int16 testValue = 16;
-    AtomicTest< int16 > int16Test(testValue);
+    AtomicTest<int16> int16Test(testValue);
     ASSERT_TRUE(int16Test.All());
 }
 
 TEST_F(AtomicGTest,Int8Test) {
     int8 testValue = 8;
-    AtomicTest< int8 > int8Test(testValue);
+    AtomicTest<int8> int8Test(testValue);
     ASSERT_TRUE(int8Test.All());
 }
 
 TEST_F(AtomicGTest,ExchangeTest) {
     int32 testValue = 32;
-    AtomicTest< int32 > exchangeTest(testValue);
+    AtomicTest<int32> exchangeTest(testValue);
     ASSERT_TRUE(exchangeTest.TestExchange());
 }
 
 TEST_F(AtomicGTest,AddTest) {
     int32 testValue = 32;
-    AtomicTest< int32 > addTest(testValue);
+    AtomicTest<int32> addTest(testValue);
     ASSERT_TRUE(addTest.TestAdd());
 }
 
 TEST_F(AtomicGTest,SubTest) {
     int32 testValue = 32;
-    AtomicTest< int32 > subTest(testValue);
+    AtomicTest<int32> subTest(testValue);
     ASSERT_TRUE(subTest.TestSub());
 }
+
