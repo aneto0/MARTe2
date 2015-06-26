95c810f0b6fbed26ccd5e7d836c432555b2abf37
Giuseppe Ferrò <frgpp01@uniroma2.it>
2015-06-26 11:59:53 +0200

    Added Processor and ProcessorType GTests. #122 #123

--- Test/GTest/ProcessorTypeGTest.cpp
+++ Test/GTest/ProcessorTypeGTest.cpp
@@ -1,16 +1,50 @@
-/*
- *  ProcessorGTest.cpp
+/**
+ * @file ProcessorTypeGTest.cpp
+ * @brief Source file for class ProcessorTypeGTest
+ * @date 25/giu/2015
+ * @author Giuseppe Ferrò
  *
- *  Created on: Feb 13, 2015
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This source file contains the definition of all the methods for
+ * the class ProcessorTypeGTest (public, protected, and private). Be aware that some 
+ * methods, such as those inline could be defined on the header file, instead.
  */
+
+/*---------------------------------------------------------------------------*/
+/*                         Standard header includes                          */
+/*---------------------------------------------------------------------------*/
 #include <limits.h>
+
+/*---------------------------------------------------------------------------*/
+/*                         Project header includes                           */
+/*---------------------------------------------------------------------------*/
+
 #include "gtest/gtest.h"
 #include "ProcessorTypeTest.h"
+
+/*---------------------------------------------------------------------------*/
+/*                           Static definitions                              */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                           Method definitions                              */
+/*---------------------------------------------------------------------------*/
 
 class ProcessorTypeGTest: public ::testing::Test {
 protected:
     virtual void SetUp() {
-        // Code here will be called immediately after the constructor 
+        // Code here will be called immediately after the constructor
         // (right before each test).
 
     }
@@ -50,3 +84,4 @@ TEST_F(ProcessorTypeGTest,TestConstructors) {
     ProcessorTypeTest processorTypeTest;
     ASSERT_TRUE(processorTypeTest.TestConstructors());
 }
+
