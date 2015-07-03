11e4547e800bb80a1cd69919f0c3831a1251d609
André Neto <Andre.Neto@f4e.europa.eu>
2015-07-03 14:18:07 +0200

    Synchronise MARTe2-doc using rsync (as opposed to git).

--- EclipseLint.sh
+++ EclipseLint.sh
@@ -2,7 +2,7 @@
 HOST=flexelint.codac.local
 REMOTE_DIR_CODE=MARTe2-dev
 REMOTE_DIR_DOC=MARTe2-doc
-LOCAL_DIR_DOC=../MARTe2-doc
+LOCAL_DIR_DOC=../MARTe2-doc/.
 rsync -avz --delete -e ssh . $USER@$HOST:$REMOTE_DIR_CODE
 rsync -avz --delete -e ssh $LOCAL_DIR_DOC $USER@$HOST:$REMOTE_DIR_DOC
 ssh $USER@$HOST "/opt/FlexeLint/flint -i $REMOTE_DIR_CODE/Source/Core/L0Portability/ -i /opt/FlexeLint/supp/lnt -i $REMOTE_DIR_DOC/Assets/Configuration/ -v marte_flint_eclipse.lnt"
