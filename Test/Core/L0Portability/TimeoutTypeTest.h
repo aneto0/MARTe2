889c8b3371a83510039e3a5ca0afb740e6b81dc6
Llorenç Capellà <lcapella@indra.es>
2015-07-23 15:12:51 +0200

    US#163 Lint: TimeoutType Tests
    Impement the unit test for the TimeoutType class.
    
    The files created are:
    	* TimeoutTypeTest.cpp
    	* TimeoutTypeTEst.h
    	* TimeoutTypeGTest.cpp
    	
    Moreover the Makesfile.inc are modified to include this tests.

---
+++ Test/Core/L0Portability/TimeoutTypeTest.h
@@ -0,0 +1,142 @@
+/**
+ * @file TimeoutTypeTest.h
+ * @brief Header file for class TimeoutTypeTest
+ * @date 23/07/2015
+ * @author llcapella
+ *
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This header file contains the declaration of the class TimeoutTypeTest
+ * with all of its public, protected and private members. It may also include
+ * definitions for inline methods which need to be visible to the compiler.
+ */
+
+#ifndef TEST_CORE_L0PORTABILITY_TIMEOUTTYPETEST_H_
+#define 		TEST_CORE_L0PORTABILITY_TIMEOUTTYPETEST_H_
+
+/*---------------------------------------------------------------------------*/
+/*                        Standard header includes                           */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                        Project header includes                            */
+/*---------------------------------------------------------------------------*/
+#include "TimeoutType.h"
+/*---------------------------------------------------------------------------*/
+/*                           Class declaration                               */
+/*---------------------------------------------------------------------------*/
+/**
+ * @brief A class to test all the TimeoutType functions.
+ */
+class TimeoutTypeTest {
+public:
+    TimeoutTypeTest();
+    virtual ~TimeoutTypeTest();
+
+    /**
+     * @brief Test the constructor of the TimeoutType class
+     * @detail declare a TimeoutType variable using the default time.
+     * @return true if the TimeoutType::msecTimeout = 0xFFFFFFFFu
+     */
+    bool TestConstructorDefault();
+
+    /**
+     * @brief Test the constructor of the TimeoutType class
+     * @detail declare a TimeoutType variable specifying the time.
+     * @param[in] msec is the time in milliseconds.
+     * @return true if the TimeoutType::msecTimeout = 0xFFFFFFFFu
+     */
+    bool TestConstructorNotDefault(uint32 msec);
+
+    /**
+     * @brief Test TimeoutType::SetTimeoutSec() function.
+     * @detail creat a timeoutType variable by default and then set the msecTimeout using
+     * TimeoutType::SetTimeoutSec.
+     * @param[in] sec is the time in seconds.
+     * @return true if theTimeoutType::msecTimeout is the  expected value.
+     */
+    bool TestSetTimeoutSec(double sec);
+
+    /**
+     * @brief Test TimeoutType::SetTimeoutHighResolutionTimerTicks() function.
+     * @detail Set the TimeoutType::msecTimeout using ticks (both ticks > 0 and ticks < 0) and then
+     * compare TimeoutType::msecTimeout with an expected value.
+     * @return true if the TimeoutType::msecTimeout is the expected value when ticks > 0 and 0 when
+     * ticks < 0.
+     */
+    bool TestSetTimeoutHighResolutionTimerTicks();
+
+    /**
+     * @brief Test TimeoutType::TestHighResolutionTimerTicks() function.
+     * @detail Set the TimeoutType::msecTimeout and then get it using TestHighResolutionTimerTicks
+     * @return true if the TimeoutType::TestHighResolutionTimerTicks() returns the expected ticks.
+     */
+    bool TestHighResolutionTimerTicks();
+
+    /**
+     * @brief Test -= operator.
+     * @detail Use -= operator and compare the result with the expected value. The two possibilities are
+     * tested: the result is positive (normal case) and the result is negative (in this case
+     * msecTimeout = 0)
+     * @return true if the result of the -= operation is as expected.
+     */
+    bool TestAutoSubstractOPerator();
+
+    /**
+     * @brief Test == operator.
+     * @detail compare 3 TimeoutType two of them are equals and the other are different.
+     * @return true if the comparison is true when the equal TimeoutType variables are compared and
+     * false when they are different.
+     */
+    bool TestEqualComparison();
+
+    /**
+     * @brief Test != operator.
+     * @detail compare 3 TimeoutType two of them are equals and the other are different.
+     * @return true if the comparison is false when the equal TimeoutType variables are compared and
+     * true when they are different.
+     */
+    bool TestDiffComparison();
+
+    /**
+     * @brief Test = operator.
+     * @detail copy timeout1 to timeout2 using = operator.
+     * @return true if timeout1 = timeout2.
+     */
+    bool TestAssingOperator();
+
+    /**
+     * @brief Test TimeoutType::TestIsFinite() function.
+     * @detail create two TimeoutType variables; one with a finite msecTimeout and another with
+     * infinite msceTimeout
+     * @return true if TimeoutType::TestIsFinite() returns true when msecTimeout is finite and returns
+     * false when the msecTimeout is infinite.
+     */
+    bool TestIsFinite();
+
+    /**
+     * @brief Test TimeoutType::GetTimeoutMSec() function.
+     * @detail create TimeoutType using default constructor and then check the msecTimeout
+     * @return true if TimeoutType::TestGetTimeoutMSec() returns the expected msecTimeout.
+     */
+    bool TestGetTimeoutMSec();
+
+    bool retValue;
+};
+
+/*---------------------------------------------------------------------------*/
+/*                        Inline method definitions                          */
+/*---------------------------------------------------------------------------*/
+
+#endif /* TEST_CORE_L0PORTABILITY_TIMEOUTTYPETEST_H_ */
+
---
+++ Test/GTest/TimeoutTypeGTest.cpp
@@ -0,0 +1,95 @@
+/**
+ * @file TimeoutTypeGTest.cpp
+ * @brief Source file for class TimeoutGTest
+ * @date 23/07/2015
+ * @author llcapella
+ *
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This source file contains the definition of all the methods for
+ * the class TimeoutGTest (public, protected, and private). Be aware that some 
+ * methods, such as those inline could be defined on the header file, instead.
+ */
+
+/*---------------------------------------------------------------------------*/
+/*                         Standard header includes                          */
+/*---------------------------------------------------------------------------*/
+#include <limits.h>
+/*---------------------------------------------------------------------------*/
+/*                         Project header includes                           */
+/*---------------------------------------------------------------------------*/
+#include "gtest/gtest.h"
+#include "TimeoutTypeTest.h"
+/*---------------------------------------------------------------------------*/
+/*                           Static definitions                              */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                           Method definitions                              */
+/*---------------------------------------------------------------------------*/
+
+TEST(TimeoutTypeGTest,TestConstructorDefault) {
+    TimeoutTypeTest timeoutTypeTest;
+    ASSERT_TRUE(timeoutTypeTest.TestConstructorDefault());
+}
+
+TEST(TimeoutTypeGTest,TestConstructorNotDefault) {
+    TimeoutTypeTest timeoutTypeTest;
+    ASSERT_TRUE(timeoutTypeTest.TestConstructorNotDefault(10));
+}
+
+TEST(TimeoutTypeGTest,TestSetTimeoutSec) {
+    TimeoutTypeTest timeoutTypeTest;
+    ASSERT_TRUE(timeoutTypeTest.TestSetTimeoutSec(1));
+}
+
+TEST(TimeoutTypeGTest,TestSetTimeoutHighResolutionTimerTicks) {
+    TimeoutTypeTest timeoutTypeTest;
+    ASSERT_TRUE(timeoutTypeTest.TestSetTimeoutHighResolutionTimerTicks());
+}
+
+TEST(TimeoutTypeGTest,TestHighResolutionTimerTicks) {
+    TimeoutTypeTest timeoutTypeTest;
+    ASSERT_TRUE(timeoutTypeTest.TestHighResolutionTimerTicks());
+}
+
+TEST(TimeoutTypeGTest,TestAutoSubstractOPerator) {
+    TimeoutTypeTest timeoutTypeTest;
+    ASSERT_TRUE(timeoutTypeTest.TestAutoSubstractOPerator());
+}
+
+TEST(TimeoutTypeGTest,TestEqualComparison) {
+    TimeoutTypeTest timeoutTypeTest;
+    ASSERT_TRUE(timeoutTypeTest.TestEqualComparison());
+}
+
+TEST(TimeoutTypeGTest,TestDiffComparison) {
+    TimeoutTypeTest timeoutTypeTest;
+    ASSERT_TRUE(timeoutTypeTest.TestDiffComparison());
+}
+
+TEST(TimeoutTypeGTest,TestAssingOperator) {
+    TimeoutTypeTest timeoutTypeTest;
+    ASSERT_TRUE(timeoutTypeTest.TestAssingOperator());
+}
+
+TEST(TimeoutTypeGTest,TestIsFinite) {
+    TimeoutTypeTest timeoutTypeTest;
+    ASSERT_TRUE(timeoutTypeTest.TestIsFinite());
+}
+
+TEST(TimeoutTypeGTest,TestGetTimeoutMSec) {
+    TimeoutTypeTest timeoutTypeTest;
+    ASSERT_TRUE(timeoutTypeTest.TestGetTimeoutMSec());
+}
+
