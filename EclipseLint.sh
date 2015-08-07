3c2e1d8f7e710c3b7d541b408eda946567b95133
gferro <Giuseppe.Ferro@uniroma2.it>
2015-08-07 18:22:55 +0200

    merge commit

--- EclipseLint.sh
+++ EclipseLint.sh
@@ -2,4 +2,4 @@
 HOST=flexelint.codac.local
 REMOTE_DIR_CODE=MARTe2-dev
 rsync -avz --delete -e ssh . $USER@$HOST:$REMOTE_DIR_CODE
-ssh $USER@$HOST "/opt/FlexeLint/flint -i $REMOTE_DIR_CODE/Source/Core/L0Portability/ -i $REMOTE_DIR_CODE/Source/Core/L0Portability/OperatingSystem/Linux -i /opt/FlexeLint/supp/lnt -i $REMOTE_DIR_CODE/MakeDefaults/Lint/ -v marte_flint_eclipse.lnt | grep -v lint-linux.h"
+ssh $USER@$HOST "/opt/FlexeLint/flint -i $REMOTE_DIR_CODE/Source/Core/L0Portability/ -i $REMOTE_DIR_CODE/Source/Core/L0Portability/OperatingSystem/Linux -i $REMOTE_DIR_CODE/Source/Core/L1Objects/ -i /opt/FlexeLint/supp/lnt -i $REMOTE_DIR_CODE/MakeDefaults/Lint/ -v marte_flint_eclipse.lnt | grep -v lint-linux.h"
