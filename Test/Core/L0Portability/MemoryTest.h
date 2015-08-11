18d747686e18889b0b29c2c6da8814a784af3878
Andre Neto <Andre.Neto@f4e.europa.eu>
2015-08-11 18:10:05 +0200

    Minor updates to the documentation.

--- Test/Core/L0Portability/MemoryTest.h
+++ Test/Core/L0Portability/MemoryTest.h
@@ -38,7 +38,7 @@
 /*---------------------------------------------------------------------------*/
 
 /**
- * @brief A class to testing Memory functions.
+ * @brief A class to test the Memory functions.
  */
 class MemoryTest {
 
@@ -48,7 +48,7 @@ public:
      * @brief Tests the Memory::Malloc function.
      * @details Allocates blocks of memory with different sizes and checks that the return pointer is different than zero.
      * @param[in] size is the number of integers to allocate.
-     * @return true if all the tests returns as expected, false otherwise.
+     * @return true if the memory can be successfully allocated and freed.
      */
     bool TestMalloc(uint32 size);
 
@@ -65,16 +65,16 @@ public:
      * @details Checks if the previous allocated memory is preserved after a realloc.
      * @param[in] size1 is the number of integers to allocate with initial malloc.
      * @param[in] size2 is the additional memory which must be allocated.
-     * @return true if all tests return as expected, false otherwise.
+     * @return true if the first block of memory can be successfully allocated and a subsequent
+     * call to realloc, successfully allocates new memory and preserves the original memory block.
      */
-    bool TestRealloc(uint32 size1,
-                     uint32 size2);
+    bool TestRealloc(uint32 size1, uint32 size2);
 
 
     /**
      * @brief Tests the Memory::Realloc function.
-     * @details Checks the function passing size = 0 (Memory::Free behavior).
-     * @return true if all tests return as expected, false otherwise.
+     * @details Checks the function with a size = 0 argument (Memory::Free behavior).
+     * @return true if the function behaves like the Memory::Free.
      */
     bool TestReallocZeroSize();
 
@@ -83,116 +83,104 @@ public:
      * @brief Tests the Memory::Realloc function.
      * @details Checks the function passing NULL pointer input (Memory::Malloc) behavior.
      * @param[in] size is the number of integers to allocate.
-     * @return true if all tests return as expected, false otherwise.
+     * @return true if the function behaves like the Memory::Malloc.
      */
     bool TestReallocNullPointer(uint32 size);
 
 
     /**
      * @brief Tests the Memory::StringDup function.
-     * @details Checks if the string is duplicated correctly and if the function returns NULL in case of NULL input.
      * @param[in] s is the string to duplicate.
-     * @return true if the tests return as expected, false otherwise.
+     * @return true if the string is duplicated correctly and if the function returns NULL in case of NULL input.
      */
     bool TestStringDup(const char8 *s);
 
     /**
      * @brief Tests the Memory::Check function.
-     * @details Allocates a chunk of memory and checks if the function returns true on the memory allocated.
-     * Then checks if the function returns false in case of null pointer input.
      * @param[in] size is the size which must be allocated.
-     * @return true if all tests return as expected, false otherwise.
+     * @return true if the Memory::Check function returns true on the memory previously allocated and false in
+     * case of a NULL pointer input
      */
     bool TestCheck(uint32 size);
 
     /**
      * @brief Tests the Memory::Copy function.
-     * @details Copies a memory area checking if the number of size specified are copied correctly.
-     * @return true if the tests return as expected, false otherwise.
+     * @details Copies an area of memory checking if the number of size specified are copied correctly.
+     * @return true if the memory is copied correctly.
      */
     bool TestCopy();
 
     /**
-     * @brief Tests the Memory::Copy function.
-     * @details Calls the function checking if the it returns true but doing nothing in case of size = 0.
-     * @return true if the tests return as expected, false otherwise.
+     * @brief Tests the Memory::Copy function with size = 0.
+     * @return true if the Memory::Copy function returns true and no data is copied.
      */
     bool TestCopyZeroSize();
 
     /**
-     * @brief Tests the Memory::Copy function.
-     * @details Calls the function with null pointers as inputs and checks if it returns false.
-     * @return true if the tests return as expected, false otherwise.
+     * @brief Tests the Memory::Copy function with a NULL pointer.
+     * @return true if the Memory::Copy function returns false.
      */
     bool TestCopyNullPointer();
 
     /**
-     * @brief Tests the Memory::Copy function.
-     * @details Copies a memory area checking if the number of size specified are copied correctly.
-     * @return true if the tests return as expected, false otherwise.
+     * @brief Tests the Memory::Move function.
+     * @return true if an area of memory can be successfully moved.
      */
     bool TestMove();
 
     /**
-     * @brief Tests the Memory::Copy function.
-     * @details Calls the function checking if the it returns true but doing nothing in case of size = 0.
-     * @return true if the tests return as expected, false otherwise.
+     * @brief Tests the Memory::Move function with size = 0.
+     * @return true if the Memory::Move function returns true and no data is moved.
      */
     bool TestMoveZeroSize();
 
     /**
-     * @brief Tests the Memory::Copy function.
-     * @details Calls the function with null pointers as inputs and checks if it returns false.
-     * @return true if the tests return as expected, false otherwise.
+     * @brief Tests the Memory::Move function with a NULL pointer.
+     * @return true if the Memory::Move function returns false.
      */
     bool TestMoveNullPointer();
 
     /**
      * @brief Tests the Memory::Compare function.
-     * @details Compares two memory areas checking if the result is correct.
-     * @return true if the tests return as expected, false otherwise.
+     * @details Exercises the Compares Memory::Compare function with several patterns.
+     * @return true if all the patterns return as expected.
      */
     bool TestCompare();
 
     /**
      * @brief Tests the Memory::Set function.
-     * @details Sets a character in the beginning of a memory area and another character at its end. Then checks if the memory is set as expected.
-     * @return true if the tests return as expected, false otherwise.
+     * @details Sets a list of characters in the beginning of a memory area and another list of characters at its end.
+     * @return true if the memory is set as specified above.
      */
     bool TestSet();
 
     /**
-     * @brief Tests the Memory::Set function.
-     * @details Calls the function passing size = 0 and checks if it returns true but doing nothing.
-     * @return true if the tests return as expected, false otherwise.
+     * @brief Tests the Memory::Set function with size = 0.
+     * @return true if the Memory::Set function returns true and no data is moved.
      */
     bool TestSetZeroSize();
 
     /**
-     * @brief Tests the Memory::Search function.
-     * @details Searches a character saved on a memory area checking if the function returns true.
-     * @return true if the tests return as expected, false otherwise.
+     * @brief Tests the Memory::Search function, searching for a character saved on a memory area .
+     * @return true if the the character can be found.
      */
     bool TestSearch();
 
     /**
-     * @brief Tests the Memory::Search function.
-     * @details Checks if the function returns false when the character is not in the buffer.
-     * @return true if the tests return as expected, false otherwise.
+     * @brief Tests the Memory::Search function, searching for a non-existing character on a memory area .
+     * @return true if the the character cannot be found.
      */
     bool TestSearchNotInBuffer();
 
     /**
-     * @brief Tests the Memory::Search function.
-     * @details Checks if the function returns false if the character is in the buffer but the specified size argument is minor than its position.
-     * @return true if the tests return as expected, false otherwise.
+     * @brief Tests the Memory::Search function looking for a character that exists but is out of the searching bounds.
+     * @return true if the Memory::Search returns false.
      */
     bool TestSearchOutOfRanges();
 
     /**
-     * @brief Tests the Memory::Search function.
-     * @details Checks if the function returns false in case of size = 0 passed by argument.
-     * @return true if the tests return as expected, false otherwise.
+     * @brief Tests the Memory::Search function with size = 0.
+     * @return true if the Memory::Search function returns false for size = 0.
      */
     bool TestSearchZeroSize();
 
