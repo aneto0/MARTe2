d80c3de3eb73336d356fd61c95395e4741435b3d
filippo sartori <filippo.sartori@f4e.europa.eu>
2015-08-13 15:54:07 +0200

    New rule to generate file .cpp from ../Generic/*_Generic.cpp

---
+++ Source/Core/L0Portability/OperatingSystem/Generic/StandardHeap_Generic.cpp
@@ -0,0 +1,121 @@
+/**
+ * @file StandardHeap_Generic.cpp
+ * @brief Source file for class StandardHeap_Generic
+ * @date Aug 13, 2015
+ * @author fsartori
+ *
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This source file contains the definition of all the methods for
+ * the class StandardHeap_Generic (public, protected, and private). Be aware that some 
+ * methods, such as those inline could be defined on the header file, instead.
+ */
+
+/*---------------------------------------------------------------------------*/
+/*                         Standard header includes                          */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                         Project header includes                           */
+/*---------------------------------------------------------------------------*/
+
+#include "StandardHeap.h"
+
+/*---------------------------------------------------------------------------*/
+/*                           Static definitions                              */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                           Method definitions                              */
+/*---------------------------------------------------------------------------*/
+
+namespace HeapManager {
+/**
+ * @brief constructor
+ */
+StandardHeap::StandardHeap(){
+
+    /** initialise memory addresses to NULL as we have no way to obtain this information until malloc is called */
+    firstAddress = 0U;
+    lastAddress  = 0U;
+}
+/**
+ * @brief destructor
+ */
+StandardHeap::~StandardHeap(){
+    lastAddress  = 0U;
+    firstAddress = 0U;
+};
+
+/**
+ * @brief allocates size bytes of data in the heap. Maximum allocated size is 4Gbytes
+ * @return a pointer to the allocated memory or NULL if the allocation fails.
+ */
+void *StandardHeap::Malloc(const uint32 &size){
+    //void *pointer = malloc(size);
+    void *pointer = new char8[size];
+
+    /*lint -e{9091} -e{923} the casting from pointer type to integer type is required
+     * in order to be able to update the range of addresses provided by this heap
+     * uintp is an integer type that has by design the same span as a pointer in all systems*/
+    uintp address = reinterpret_cast<uintp>(pointer);
+    if ((firstAddress > address ) || (firstAddress == 0U)){
+        firstAddress = address;
+    }
+    address += size;
+    if ((lastAddress  < address ) || (lastAddress  == 0U)) {
+        lastAddress = address;
+    }
+
+    return pointer;
+
+}
+
+/**
+ * @brief free the pointer data and its associated memory.
+ * @param data the data to be freed.
+ */
+void StandardHeap::Free(void *&data){
+
+    delete[] (reinterpret_cast<char8 *>(data));
+    data = NULL_PTR(void *);
+//    free(data);
+}
+
+/**
+ * @brief start of range of memory addresses served by this heap.
+ * @return first memory address
+ */
+uintp StandardHeap::FirstAddress()const {
+    return firstAddress;
+}
+
+/**
+ * @brief end (inclusive) of range of memory addresses served by this heap.
+ * @return last memory address
+ */
+uintp StandardHeap::LastAddress()const {
+    return lastAddress;
+}
+
+/**
+ * @brief Returns the name of the heap
+ * @return name of the heap
+ */
+const char8 *StandardHeap::Name()const {
+    return "StandardHeap";
+}
+
+}
+
+	
