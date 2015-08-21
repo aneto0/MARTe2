728629e31db5ea1d52372593a83ae8bd26764601
gferro <Giuseppe.Ferro@uniroma2.it>
2015-08-21 13:24:15 +0200

    List of changes: - FastMath now is a namespace.

--- Source/Core/L0Portability/FastMath.h
+++ Source/Core/L0Portability/FastMath.h
@@ -42,74 +42,72 @@
 /**
  * @brief Performs mathematical operations using fast processor instructions.
  */
-class FastMath {
-
-public:
+namespace FastMath {
 
     /**
      * The constant for e
      */
-    static const float64 E;
+    static const float64 E = 2.7182818284590452354;
 
     /**
      * The constant for log2(e)
      */
-    static const float64 LOG2E;
+    static const float64 LOG2E = 1.4426950408889634074;
 
     /**
      * The constant for log10(e)
      */
-    static const float64 LOG10E;
+    static const float64 LOG10E = 0.43429448190325182765;
 
     /**
      * The constant for log(2)
      */
-    static const float64 LN2;
+    static const float64 LN2 = 0.69314718055994530942;
 
     /**
      * The constant for log(10)
      */
-    static const float64 LN10;
+    static const float64 LN10 = 2.30258509299404568402;
 
     /**
      * The constant for pi
      */
-    static const float64 PI;
+    static const float64 PI = 3.14159265358979323846;
 
     /**
      * The constant for pi/2
      */
-    static const float64 PI_2;
+    static const float64 PI_2 = 1.57079632679489661923;
 
     /**
      * The constant for pi/4
      */
-    static const float64 PI_4;
+    static const float64 PI_4 = 0.78539816339744830962;
 
     /**
      * The constant for 1/pi
      */
-    static const float64 ONE_PI;
+    static const float64 ONE_PI = 0.31830988618379067154;
 
     /**
      * The constant for 2/pi
      */
-    static const float64 TWO_PI;
+    static const float64 TWO_PI = 0.63661977236758134308;
 
     /**
      * The constant for 2/sqrt(pi)
      */
-    static const float64 TWO_SQRTPI;
+    static const float64 TWO_SQRTPI = 1.12837916709551257390;
 
     /**
      * The constant for sqrt(2)
      */
-    static const float64 SQRT2;
+    static const float64 SQRT2 = 1.41421356237309504880;
 
     /**
      * The constant for sqrt(1/2)
      */
-    static const float64 SQRT1_2;
+    static const float64 SQRT1_2 = 0.70710678118654752440;
 
     /**
      * @brief Converts a float to an integer using a processor instruction.
