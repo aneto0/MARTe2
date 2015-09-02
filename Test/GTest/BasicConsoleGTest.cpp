f235777a792c21b454313a033179891854466e05
Ivan Herrero Molina <ivan.herrero@gtd.eu>
2015-09-02 17:40:25 +0200

    Fix BasicConsoleGTest (all tests were commented/deactivated in a previous commit).

--- Test/GTest/BasicConsoleGTest.cpp
+++ Test/GTest/BasicConsoleGTest.cpp
@@ -42,138 +42,138 @@
 /*                           Method definitions                              */
 /*---------------------------------------------------------------------------*/
 
-//TEST(BasicConsoleGTest,TestConstructor) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestConstructor());
-//}
-//
-//
-//TEST(BasicConsoleGTest,TestOpenModeDefault) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestOpenModeDefault());
-//}
-//
-//TEST(BasicConsoleGTest,TestOpenModeCreateNewBuffer) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestOpenModeCreateNewBuffer());
-//}
-//
-//TEST(BasicConsoleGTest,TestOpenModePerformCharacterInput) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestOpenModePerformCharacterInput());
-//}
-//
-//TEST(BasicConsoleGTest,TestOpenModeDisableControlBreak) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestOpenModeDisableControlBreak());
-//}
-//
-//TEST(BasicConsoleGTest,TestOpenModeEnablePaging) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestOpenModeEnablePaging());
-//}
-//
-//TEST(BasicConsoleGTest,TestGetOpeningMode) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestGetOpeningMode());
-//}
-//
-//TEST(BasicConsoleGTest,TestClose) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestClose());
-//}
-//
-//TEST(BasicConsoleGTest,TestWriteCheckReturn) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestWriteCheckReturn("ABC\nDE\n", 1));
-//}
-//
-//TEST(BasicConsoleGTest,TestWriteNullString) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestWriteNullString());
-//}
-//
-//TEST(BasicConsoleGTest,TestWriteExactSize) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestWriteExactSize("abcd"));
-//}
-//
-//TEST(BasicConsoleGTest,TestWriteSmallSize) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestWriteSmallSize("abcd"));
-//}
-//
-//TEST(BasicConsoleGTest,TestWriteEndColumn) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestWriteEndColumn());
-//}
-//
-//TEST(BasicConsoleGTest,TestWrite2) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestWriteCheckReturn("Hello bye bye world\nWorld with spaces\n\n\nand new lines\nand\nsingle\nwords.", 0));
-//}
-//
-////This test needs user intervention. Do not uncomment for automatic tests.
-//TEST(BasicConsoleGTest,TestPaging) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestPaging(14, 15, 15));
-//}
-//
-////This test needs user intervention. Do not uncomment for automatic tests.
-//TEST(BasicConsoleGTest,TestRead) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestRead("Hello"));
-//}
-//
-//TEST(BasicConsoleGTest,TestTimeoutRead) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestTimeoutRead(100));
-//}
-//
-//
-//TEST(BasicConsoleGTest,TestSetGetSize) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestSetGetSize(100, 70));
-//}
-//
-//TEST(BasicConsoleGTest,TestSetGetWindowsSize) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestSetGetWindowSize(50, 20));
-//}
-//
-//TEST(BasicConsoleGTest,TestClear) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestClear());
-//}
-//
-////This test needs user intervention. Do not uncomment for automatic tests.
-//TEST(BasicConsoleGTest,TestPerfChar) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestPerfChar());
-//}
-//
-//TEST(BasicConsoleGTest,TestShow) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestShowBuffer());
-//}
-//TEST(BasicConsoleGTest,TestSetGetTitleBar) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestSetGetTitleBar("title"));
-//}
-//
-////This test is not supported.
-//TEST(BasicConsoleGTest,TestSetGetCursorPosition) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestSetGetCursorPosition(20, 10));
-//}
-//
-////This test is not supported.
-//TEST(BasicConsoleGTest,TestSetColour) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestSetColour(Blue, Red));
-//}
-//
-//TEST(BasicConsoleGTest,TestPlotChar) {
-//    BasicConsoleTest console;
-//    ASSERT_TRUE(console.TestPlotChar('c', Blue, Yellow, 20, 10));
-//}
+TEST(BasicConsoleGTest,TestConstructor) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestConstructor());
+}
+
+
+TEST(BasicConsoleGTest,TestOpenModeDefault) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestOpenModeDefault());
+}
+
+TEST(BasicConsoleGTest,TestOpenModeCreateNewBuffer) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestOpenModeCreateNewBuffer());
+}
+
+TEST(BasicConsoleGTest,TestOpenModePerformCharacterInput) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestOpenModePerformCharacterInput());
+}
+
+TEST(BasicConsoleGTest,TestOpenModeDisableControlBreak) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestOpenModeDisableControlBreak());
+}
+
+TEST(BasicConsoleGTest,TestOpenModeEnablePaging) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestOpenModeEnablePaging());
+}
+
+TEST(BasicConsoleGTest,TestGetOpeningMode) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestGetOpeningMode());
+}
+
+TEST(BasicConsoleGTest,TestClose) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestClose());
+}
+
+TEST(BasicConsoleGTest,TestWriteCheckReturn) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestWriteCheckReturn("ABC\nDE\n", 1));
+}
+
+TEST(BasicConsoleGTest,TestWriteNullString) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestWriteNullString());
+}
+
+TEST(BasicConsoleGTest,TestWriteExactSize) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestWriteExactSize("abcd"));
+}
+
+TEST(BasicConsoleGTest,TestWriteSmallSize) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestWriteSmallSize("abcd"));
+}
+
+TEST(BasicConsoleGTest,TestWriteEndColumn) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestWriteEndColumn());
+}
+
+TEST(BasicConsoleGTest,TestWrite2) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestWriteCheckReturn("Hello bye bye world\nWorld with spaces\n\n\nand new lines\nand\nsingle\nwords.", 0));
+}
+
+//This test needs user intervention. Do not uncomment for automatic tests.
+TEST(BasicConsoleGTest,TestPaging) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestPaging(14, 15, 15));
+}
+
+//This test needs user intervention. Do not uncomment for automatic tests.
+TEST(BasicConsoleGTest,TestRead) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestRead("Hello"));
+}
+
+TEST(BasicConsoleGTest,TestTimeoutRead) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestTimeoutRead(100));
+}
+
+
+TEST(BasicConsoleGTest,TestSetGetSize) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestSetGetSize(100, 70));
+}
+
+TEST(BasicConsoleGTest,TestSetGetWindowsSize) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestSetGetWindowSize(50, 20));
+}
+
+TEST(BasicConsoleGTest,TestClear) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestClear());
+}
+
+//This test needs user intervention. Do not uncomment for automatic tests.
+TEST(BasicConsoleGTest,TestPerfChar) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestPerfChar());
+}
+
+TEST(BasicConsoleGTest,TestShow) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestShowBuffer());
+}
+TEST(BasicConsoleGTest,TestSetGetTitleBar) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestSetGetTitleBar("title"));
+}
+
+//This test is not supported.
+TEST(BasicConsoleGTest,TestSetGetCursorPosition) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestSetGetCursorPosition(20, 10));
+}
+
+//This test is not supported.
+TEST(BasicConsoleGTest,TestSetColour) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestSetColour(Blue, Red));
+}
+
+TEST(BasicConsoleGTest,TestPlotChar) {
+    BasicConsoleTest console;
+    ASSERT_TRUE(console.TestPlotChar('c', Blue, Yellow, 20, 10));
+}
