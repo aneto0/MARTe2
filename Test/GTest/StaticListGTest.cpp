9c9a2999a1aef21c527ac707d95e842c8816a6f3
Ivan Herrero Molina <ivan.herrero@gtd.eu>
2015-09-16 18:07:19 +0200

    #225 - First version of StaticListGTest

---
+++ Test/GTest/StaticListGTest.cpp
@@ -0,0 +1,127 @@
+/**
+ * @file StaticListGTest.cpp
+ * @brief Source file for class StaticListGTest
+ * @date 07/08/2015
+ * @author Giuseppe Ferrò
+ *
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This source file contains the definition of all the methods for
+ * the class StaticListGTest (public, protected, and private). Be aware that some
+ * methods, such as those inline could be defined on the header file, instead.
+ */
+
+/*---------------------------------------------------------------------------*/
+/*                         Standard header includes                          */
+/*---------------------------------------------------------------------------*/
+
+#include <limits.h>
+
+/*---------------------------------------------------------------------------*/
+/*                         Project header includes                           */
+/*---------------------------------------------------------------------------*/
+
+#include "StaticListTest.h"
+#include "GeneralDefinitions.h"
+#include "gtest/gtest.h"
+
+using namespace Lists;
+
+/*---------------------------------------------------------------------------*/
+/*                           Static definitions                              */
+/*---------------------------------------------------------------------------*/
+
+namespace {
+
+uint32 demoValues[] = {3,7,2,5,45,3,6,7,47,3,74,5,32,91,8,66,9};
+
+}
+
+/*---------------------------------------------------------------------------*/
+/*                           Method definitions                              */
+/*---------------------------------------------------------------------------*/
+
+TEST(StaticListGTest,TestDefaultConstructor) {
+    StaticListTest<uint32,10,demoValues,sizeof(demoValues)> tester;
+    ASSERT_TRUE(tester.TestDefaultConstructor());
+}
+
+TEST(StaticListGTest,TestAddOnEmptyList) {
+    StaticListTest<uint32,10,demoValues,sizeof(demoValues)> tester;
+    ASSERT_TRUE(tester.TestAddOnEmptyList<25u>());
+}
+
+TEST(StaticListGTest,TestAddOnNonEmptyList) {
+    StaticListTest<uint32,10,demoValues,sizeof(demoValues)> tester;
+    ASSERT_TRUE(tester.TestAddOnNonEmptyList<25u>());
+}
+
+TEST(StaticListGTest,TestInsertOnEmptyList) {
+    StaticListTest<uint32,10,demoValues,sizeof(demoValues)> tester;
+    ASSERT_TRUE(tester.TestInsertOnEmptyList<25u>());
+}
+
+TEST(StaticListGTest,TestInsertOnNonEmptyListAtBeginning) {
+    StaticListTest<uint32,10,demoValues,sizeof(demoValues)> tester;
+    ASSERT_TRUE(tester.TestInsertOnNonEmptyListAtBeginning<25u>());
+}
+
+TEST(StaticListGTest,TestInsertOnNonEmptyListAtMiddle) {
+    StaticListTest<uint32,10,demoValues,sizeof(demoValues)> tester;
+    ASSERT_TRUE(tester.TestInsertOnNonEmptyListAtMiddle<25u>());
+}
+
+TEST(StaticListGTest,TestInsertOnNonEmptyListAtEnd) {
+    StaticListTest<uint32,10,demoValues,sizeof(demoValues)> tester;
+    ASSERT_TRUE(tester.TestInsertOnNonEmptyListAtEnd<25u>());
+}
+
+TEST(StaticListGTest,TestExtractAtBeginning) {
+    StaticListTest<uint32,10,demoValues,sizeof(demoValues)> tester;
+    ASSERT_TRUE(tester.TestExtractAtBeginning());
+}
+
+TEST(StaticListGTest,TestExtractAtMiddle) {
+    StaticListTest<uint32,10,demoValues,sizeof(demoValues)> tester;
+    ASSERT_TRUE(tester.TestExtractAtMiddle());
+}
+
+TEST(StaticListGTest,TestExtractAtEnd) {
+    StaticListTest<uint32,10,demoValues,sizeof(demoValues)> tester;
+    ASSERT_TRUE(tester.TestExtractAtEnd());
+}
+
+TEST(StaticListGTest,TestRemoveAtBeginning) {
+    StaticListTest<uint32,10,demoValues,sizeof(demoValues)> tester;
+    ASSERT_TRUE(tester.TestRemoveAtBeginning());
+}
+
+TEST(StaticListGTest,TestRemoveAtMiddle) {
+    StaticListTest<uint32,10,demoValues,sizeof(demoValues)> tester;
+    ASSERT_TRUE(tester.TestRemoveAtMiddle());
+}
+
+TEST(StaticListGTest,TestRemoveAtEnd) {
+    StaticListTest<uint32,10,demoValues,sizeof(demoValues)> tester;
+    ASSERT_TRUE(tester.TestRemoveAtEnd());
+}
+
+TEST(StaticListGTest,TestGetSize) {
+    StaticListTest<uint32,10,demoValues,sizeof(demoValues)> tester;
+    ASSERT_TRUE(tester.TestGetSize());
+}
+
+TEST(StaticListGTest,TestPeek) {
+    StaticListTest<uint32,10,demoValues,sizeof(demoValues)> tester;
+    ASSERT_TRUE(tester.TestPeek());
+}
