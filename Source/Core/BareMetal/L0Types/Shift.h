1288760928e3dc25013c11a73f3569d14e23d70c
Llorenç Capellà <lcapella@indra.es>
2015-10-20 10:26:28 +0200

    #237.3_shift
    Shift class imported from SVN.
    
    List of changes:
    	* MathematicSafeShift function was splited in two functions:
    MathematicRhifhtSafeShift and MathematicLeftSafeShift.
    	* The LogicalLeftShift functions were not used so they were delated
    	* Some tests were added in order test the class changes.
    	* Documentation reviewed and files fotmated

--- Source/Core/BareMetal/L0Types/Shift.h
+++ Source/Core/BareMetal/L0Types/Shift.h
@@ -32,236 +32,236 @@
 /*                        Project header includes                            */
 /*---------------------------------------------------------------------------*/
 
+#include "GeneralDefinitions.h"
+
 /*---------------------------------------------------------------------------*/
 /*                           Class declaration                               */
 /*---------------------------------------------------------------------------*/
 
-#include "GeneralDefinitions.h"
-
 namespace MARTe {
 
-/** @brief A collector of functions that implements logical and mathematic shift in safe mode.
- *
- * These methods implements shift operations in the right way, returning zero when the shift is
+/** @brief A collector of functions that implements logical and mathematical shift in safe mode.
+ * @detail These methods implements shift operations in the right way, returning zero when the shift is
  * greater than the size of the number. Furthermore they implements the logical shift which
  * does not extend the sign for negative numbers and it is very useful in many functionalities.
  */
 class Shift {
 
-private:
-
-    /** @brief Logical right shift for uint8 numbers.
-     * @param number is the number to shift.
-     * @param shift is the shift in bits.
-     * @return the number right shifted. */
-    static inline uint8 LogicalRightShift(uint8 number,
-                                          uint8 shift) {
-        return number >> shift;
-    }
-
-    /** @brief Logical right shift for uint16 numbers.
-     * @param number is the number to shift.
-     * @param shift is the shift in bits.
-     * @return the number right shifted. */
-    static inline uint16 LogicalRightShift(uint16 number,
-                                           uint8 shift) {
-        return number >> shift;
-    }
-
-    /** @brief Logical right shift for uint32 numbers.
-     * @param number is the number to shift.
-     * @param shift is the shift in bits.
-     * @return the number right shifted. */
-    static inline uint32 LogicalRightShift(uint32 number,
-                                           uint8 shift) {
-        return number >> shift;
-    }
-
-    /** @brief Logical right shift for uint64 numbers.
-     * @param number is the number to shift.
-     * @param shift is the shift in bits.
-     * @return the number right shifted. */
-    static inline uint64 LogicalRightShift(uint64 number,
-                                           uint8 shift) {
-        return number >> shift;
-    }
-
-    /** @brief Logical right shift for int8 numbers.
-     * @param number is the number to shift.
-     * @param shift is the shift in bits.
-     * @return the number right shifted. */
-    static inline int8 LogicalRightShift(int8 number,
-                                         uint8 shift) {
-        return ((uint8) number) >> shift;
-    }
-
-    /** @brief Logical right shift for int16 numbers.
-     * @param number is the number to shift.
-     * @param shift is the shift in bits.
-     * @return the number right shifted. */
-    static inline int16 LogicalRightShift(int16 number,
-                                          uint8 shift) {
-        return ((uint16) number) >> shift;
-    }
-
-    /** @brief Logical right shift for int32 numbers.
-     * @param number is the number to shift.
-     * @param shift is the shift in bits.
-     * @return the number right shifted. */
-    static inline int32 LogicalRightShift(int32 number,
-                                          uint8 shift) {
-        return ((uint32) number) >> shift;
-    }
-
-    /** @brief Logical right shift for int64 numbers.
-     * @param number is the number to shift.
-     * @param shift is the shift in bits.
-     * @return the number right shifted. */
-    static inline int64 LogicalRightShift(int64 number,
-                                          uint8 shift) {
-        return ((uint64) number) >> shift;
-    }
-
-    /** @brief Logical left shift for uint8 numbers.
-     * @param number is the number to shift.
-     * @param shift is the shift in bits.
-     * @return the number left shifted. */
-    static inline uint8 LogicalLeftShift(uint8 number,
-                                         uint8 shift) {
-        return number << shift;
-    }
-
-    /** @brief Logical left shift for uint16 numbers.
-     * @param number is the number to shift.
-     * @param shift is the shift in bits.
-     * @return the number left shifted. */
-    static inline uint16 LogicalLeftShift(uint16 number,
-                                          uint8 shift) {
-        return number << shift;
-    }
-
-    /** @brief Logical left shift for uint32 numbers.
-     * @param number is the number to shift.
-     * @param shift is the shift in bits.
-     * @return the number left shifted. */
-    static inline uint32 LogicalLeftShift(uint32 number,
-                                          uint8 shift) {
-        return number << shift;
-    }
-
-    /** @brief Logical left shift for uint64 numbers.
-     * @param number is the number to shift.
-     * @param shift is the shift in bits.
-     * @return the number left shifted. */
-    static inline uint64 LogicalLeftShift(uint64 number,
-                                          uint8 shift) {
-        return number << shift;
-    }
-
-    /** @brief Logical left shift for int8 numbers.
-     * @param number is the number to shift.
-     * @param shift is the shift in bits.
-     * @return the number left shifted. */
-    static inline int8 LogicalLeftShift(int8 number,
-                                        uint8 shift) {
-        return number << shift;
-    }
-
-    /** @brief Logical left shift for int16 numbers.
-     * @param number is the number to shift.
-     * @param shift is the shift in bits.
-     * @return the number left shifted. */
-    static inline int16 LogicalLeftShift(int16 number,
-                                         uint8 shift) {
-        return number << shift;
-    }
-
-    /** @brief Logical left shift for int32 numbers.
-     * @param number is the number to shift.
-     * @param shift is the shift in bits.
-     * @return the number left shifted. */
-    static inline int32 LogicalLeftShift(int32 number,
-                                         uint8 shift) {
-        return number << shift;
-    }
-
-    /** @brief Logical left shift for int64 numbers.
-     * @param number is the number to shift.
-     * @param shift is the shift in bits.
-     * @return the number left shifted. */
-    static inline int64 LogicalLeftShift(int64 number,
-                                         uint8 shift) {
-        return number << shift;
-    }
-
 public:
 
-    /** @brief Implementation of a template function for safe logical shifts.
-     * @param number is the number to be shifted.
-     * @param shift is the shift in bits.
-     * @return the number shifted. */
+    /**
+     * @brief implementation of a template function for safe logical right shifts.
+     * @param[in] number is the number to be shifted.
+     * @param[in] shift is the number of bits shifted.
+     * @return the number shifted.
+     */
     template<typename T>
     static inline T LogicalRightSafeShift(T number,
-                                          uint8 shift) {
+                                          uint8 shift);
 
-        uint8 bound = static_cast<uint8>(sizeof(T) * 8u);
-
-        //if |shift| is greater than the size of the number return 0.
-        if (shift >= bound) {
-            return (T) 0;
-        }
-
-        return LogicalRightShift(number, shift);
-    }
-
-    /** @brief Implementation of a template function for safe logical shifts.
-     * @param number is the number to be shifted.
-     * @param shift is the shift in bits.
-     * @return the number shifted. */
+    /**
+     * @brief implementation of a template function for safe logical shifts.
+     * @param[in] number is the number to be shifted.
+     * @param[in] shift is the number of bits shifted.
+     * @return the number shifted.
+     */
     template<typename T>
     static inline T LogicalLeftSafeShift(T number,
-                                         uint8 shift) {
+                                         uint8 shift);
 
-        uint8 bound = sizeof(T) * 8u;
-
-        //if |shift| is greater than the size of the number return 0.
-        if (shift >= bound) {
-            return (T) 0;
-        }
-
-        return number << shift;
-    }
-
-    /** @brief Implementation of a template function for safe mathematic shifts.
-     * @param number is the number to be shifted.
-     * @param shift is the shift in bits.
-     * @return the number shifted. */
+    /**
+     * @brief implementation of a template function for safe mathematical right shifts.
+     * @param[in] number is the number to be shifted.
+     * @param[in] shift is the number of bits shifted.
+     * @return the number shifted.
+     */
     template<typename T>
-    static inline T MathematicSafeShift(T number,
-                                        int8 shift) {
+    static inline T MathematicRightSafeShift(T number,
+                                             uint8 shift);
 
-        int bound = sizeof(T) * 8;
+    /**
+     * @brief implementation of a template function for safe mathematical left shifts.
+     * @param[in] number is the number to be shifted.
+     * @param[in] shift is the number of bits shifted.
+     * @return the number shifted.
+     */
+    template<typename T>
+    static inline T MathematicLeftSafeShift(T number,
+                                            uint8 shift);
 
-        //if |shift| is greater than the number size return 0.
-        if (shift >= bound || shift <= -bound) {
-            return (T) 0;
-        }
+private:
 
-        //if shift is greater than 0 d the left shift.
-        if (shift > 0)
-            return number << shift;
-        //if the shift is minor than zero do the right shift.
-        else
-            return number >> (-shift);
-    }
+    /**
+     * @brief logical right shift for uint8 numbers.
+     * @param[in] number is the number to shift.
+     * @param[in] shift is the number of bits shifted.
+     * @return the number right shifted.
+     */
+    static inline uint8 LogicalRightShift(uint8 number,
+                                          uint8 shift);
 
+    /**
+     * @brief logical right shift for uint16 numbers.
+     * @param[in] number is the number to shift.
+     * @param[in] shift is the number of bits shifted.
+     * @return the number right shifted.
+     */
+    static inline uint16 LogicalRightShift(uint16 number,
+                                           uint8 shift);
+
+    /**
+     * @brief Logical right shift for uint32 numbers.
+     * @param[in] Number is the number to shift.
+     * @param[in] shift is the number of bits shifted.
+     * @return the number right shifted.
+     */
+    static inline uint32 LogicalRightShift(uint32 number,
+                                           uint8 shift);
+
+    /**
+     * @brief logical right shift for uint64 numbers.
+     * @param[in] number is the number to shift.
+     * @param[in] shift is the shift in bits.
+     * @return the number right shifted.
+     */
+    static inline uint64 LogicalRightShift(uint64 number,
+                                           uint8 shift);
+
+    /**
+     * @brief logical right shift for int8 numbers.
+     * @param[in] number is the number to shift.
+     * @param[in] shift is the number of bits shifted.
+     * @return the number right shifted.
+     */
+    static inline int8 LogicalRightShift(int8 number,
+                                         uint8 shift);
+
+    /**
+     * @brief logical right shift for int16 numbers.
+     * @param[in] number is the number to shift.
+     * @param[in] shift is the number of bits shifted.
+     * @return the number right shifted.
+     */
+    static inline int16 LogicalRightShift(int16 number,
+                                          uint8 shift);
+
+    /**
+     * @brief logical right shift for int32 numbers.
+     * @param[in] number is the number to shift.
+     * @param[in] shift is the number of bits shifted.
+     * @return the number right shifted.
+     */
+    static inline int32 LogicalRightShift(int32 number,
+                                          uint8 shift);
+
+    /**
+     * @brief logical right shift for int64 numbers.
+     * @param[in] number is the number to shift.
+     * @param[in] shift is the number of bits shifted.
+     * @return the number right shifted.
+     */
+    static inline int64 LogicalRightShift(int64 number,
+                                          uint8 shift);
 };
-
-}
 
 /*---------------------------------------------------------------------------*/
 /*                        Inline method definitions                          */
 /*---------------------------------------------------------------------------*/
+template<typename T>
+T Shift::LogicalRightSafeShift(T number,
+                               uint8 shift) {
+
+    uint8 bound = static_cast<uint8>(sizeof(T) * 8u);
+
+    //if is greater than the size of the number return 0.
+    if (shift >= bound) {
+        return (T) 0;
+    }
+
+    return LogicalRightShift(number, shift);
+}
+
+template<typename T>
+T Shift::LogicalLeftSafeShift(T number,
+                              uint8 shift) {
+
+    uint8 bound = sizeof(T) * 8u;
+
+    //if is greater than the size of the number return 0.
+    if (shift >= bound) {
+        return (T) 0;
+    }
+
+    return number << shift;
+}
+
+template<typename T>
+T Shift::MathematicRightSafeShift(T number,
+                                  uint8 shift) {
+
+    int bound = sizeof(T) * 8;
+
+    //if is greater than the number size return 0.
+    if (shift >= bound) {
+        return (T) 0;
+    }
+    return number >> shift;
+}
+
+template<typename T>
+T Shift::MathematicLeftSafeShift(T number,
+                                 uint8 shift) {
+
+    int bound = sizeof(T) * 8;
+
+    //if is greater than the number size return 0.
+    if (shift >= bound) {
+        return (T) 0;
+    }
+    return number << shift;
+}
+
+uint8 Shift::LogicalRightShift(uint8 number,
+                               uint8 shift) {
+    return number >> shift;
+}
+
+uint16 Shift::LogicalRightShift(uint16 number,
+                                uint8 shift) {
+    return number >> shift;
+}
+
+uint32 Shift::LogicalRightShift(uint32 number,
+                                uint8 shift) {
+    return number >> shift;
+}
+
+uint64 Shift::LogicalRightShift(uint64 number,
+                                uint8 shift) {
+    return number >> shift;
+}
+
+int8 Shift::LogicalRightShift(int8 number,
+                              uint8 shift) {
+    return ((uint8) number) >> shift;
+}
+
+int16 Shift::LogicalRightShift(int16 number,
+                               uint8 shift) {
+    return ((uint16) number) >> shift;
+}
+
+int32 Shift::LogicalRightShift(int32 number,
+                               uint8 shift) {
+    return ((uint32) number) >> shift;
+}
+
+int64 Shift::LogicalRightShift(int64 number,
+                               uint8 shift) {
+    return ((uint64) number) >> shift;
+}
+
+}
 
 #endif /* L0TYPES_SHIFT_H_ */
 
--- Test/GTest/ShiftGTest.cpp
+++ Test/GTest/ShiftGTest.cpp
@@ -1,41 +1,185 @@
-/*
- *  ShiftGTest.cpp
+/**
+ * @file ShiftGTest.cpp
+ * @brief Source file for class ShiftGTest
+ * @date 19/10/2015
+ * @author Llorenç Capellà
  *
- *  Created on: Feb 5, 2015
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This source file contains the definition of all the methods for
+ * the class ShiftGTest (public, protected, and private). Be aware that some 
+ * methods, such as those inline could be defined on the header file, instead.
  */
+
+/*---------------------------------------------------------------------------*/
+/*                         Standard header includes                          */
+/*---------------------------------------------------------------------------*/
+
 #include <limits.h>
+
+/*---------------------------------------------------------------------------*/
+/*                         Project header includes                           */
+/*---------------------------------------------------------------------------*/
+
 #include "gtest/gtest.h"
-//#include "SafeShiftTest.h"
+#include "ShiftTest.h"
+#include "GeneralDefinitions.h"
 
-class SafeShiftGTest: public ::testing::Test {
-protected:
-    virtual void SetUp() {
-        // Code here will be called immediately after the constructor
-        // (right before each test).
-    }
+/*---------------------------------------------------------------------------*/
+/*                           Static definitions                              */
+/*---------------------------------------------------------------------------*/
 
-    virtual void TearDown() {
-        // Code here will be called immediately after each test
-        // (right before the destructor).
-    }
-};
+/*---------------------------------------------------------------------------*/
+/*                           Method definitions                              */
+/*---------------------------------------------------------------------------*/
 
-TEST_F(SafeShiftGTest,TestLogicalRightShift) {
-    SafeShiftTest myShift;
-    ASSERT_TRUE(myShift.TestLogicalRightShift());
+using namespace MARTe;
+
+TEST(ShiftGTest, TestLogicalRightSaveShiftPositiveInt8) {
+    ShiftTest myShift;
+    int8 number = 8;
+    ASSERT_TRUE(myShift.TestLogicalRightSaveShift(number));
 }
 
-TEST_F(SafeShiftGTest,TestLogicalLeftShift) {
-    SafeShiftTest myShift;
-    ASSERT_TRUE(myShift.TestLogicalLeftShift());
+TEST(ShiftGTest, TestLogicalRightSaveShiftNegativeInt8) {
+    ShiftTest myShift;
+    int8 number = -8;
+    ASSERT_TRUE(myShift.TestLogicalRightSaveShift(number));
 }
 
-TEST_F(SafeShiftGTest,TestMathematicRightShift) {
-    SafeShiftTest myShift;
-    ASSERT_TRUE(myShift.TestMathematicRightShift());
+TEST(ShiftGTest, TestLogicalRightSaveShiftUint8) {
+    ShiftTest myShift;
+    uint8 number = 8;
+    ASSERT_TRUE(myShift.TestLogicalRightSaveShift(number));
 }
 
-TEST_F(SafeShiftGTest,TestMathematicLeftShift) {
-    SafeShiftTest myShift;
-    ASSERT_TRUE(myShift.TestMathematicLeftShift());
+TEST(ShiftGTest, TestLogicalRightSaveShiftUint16) {
+    ShiftTest myShift;
+    uint16 number = 12345;
+    ASSERT_TRUE(myShift.TestLogicalRightSaveShift(number));
 }
+
+TEST(ShiftGTest, TestLogicalRightSaveShiftInt16) {
+    ShiftTest myShift;
+    int16 number = 12345;
+    ASSERT_TRUE(myShift.TestLogicalRightSaveShift(number));
+}
+
+TEST(ShiftGTest, TestLogicalRightSaveShiftUint32) {
+    ShiftTest myShift;
+    uint32 number = 12345;
+    ASSERT_TRUE(myShift.TestLogicalRightSaveShift(number));
+}
+
+TEST(ShiftGTest, TestLogicalRightSaveShiftInt32) {
+    ShiftTest myShift;
+    int32 number = 12345;
+    ASSERT_TRUE(myShift.TestLogicalRightSaveShift(number));
+}
+
+TEST(ShiftGTest, TestLogicalRightSaveShiftUint64) {
+     ShiftTest myShift;
+     uint64 number = 1234567;
+     ASSERT_TRUE(myShift.TestLogicalRightSaveShift(number));
+}
+
+TEST(ShiftGTest, TestLogicalRightSaveShiftInt64) {
+     ShiftTest myShift;
+     int64 number = 1234567;
+     ASSERT_TRUE(myShift.TestLogicalRightSaveShift(number));
+}
+
+TEST(ShiftGTest, TestLogicalRightSaveShiftPositiveInt64) {
+    ShiftTest myShift;
+    int64 number = 123;
+    ASSERT_TRUE(myShift.TestLogicalRightSaveShift(number));
+}
+
+TEST(ShiftGTest, TestLogicalRightSaveShiftNegativeInt64) {
+    ShiftTest myShift;
+    int8 number = -123;
+    ASSERT_TRUE(myShift.TestLogicalRightSaveShift(number));
+}
+
+TEST(ShiftGTest,TestLogicalLeftSaveShiftPositiveInt8) {
+    ShiftTest myShift;
+    int8 number = 15;
+    ASSERT_TRUE(myShift.TestLogicalLeftSaveShift(number));
+}
+
+TEST(ShiftGTest,TestLogicalLeftSaveShiftNegativeInt8) {
+    ShiftTest myShift;
+    int8 number = -15;
+    ASSERT_TRUE(myShift.TestLogicalLeftSaveShift(number));
+}
+
+TEST(ShiftGTest,TestLogicalLeftSaveShiftPositiveInt64) {
+    ShiftTest myShift;
+    int64 number = 23489123;
+    ASSERT_TRUE(myShift.TestLogicalLeftSaveShift(number));
+}
+
+TEST(ShiftGTest,TestLogicalLeftSaveShiftNegativeInt64) {
+    ShiftTest myShift;
+    int64 number = -23489123;
+    ASSERT_TRUE(myShift.TestLogicalLeftSaveShift(number));
+}
+
+TEST(ShiftGTest,TestMathematicRightSafeShiftPositiveInt8) {
+    ShiftTest myShift;
+    int8 number = 123;
+    ASSERT_TRUE(myShift.TestMathematicRightSafeShift(number));
+}
+
+TEST(ShiftGTest,TestMathematicRightSafeShiftNegativeInt8) {
+    ShiftTest myShift;
+    int8 number = -123;
+    ASSERT_TRUE(myShift.TestMathematicRightSafeShift(number));
+}
+
+TEST(ShiftGTest,TestMathematicRightSafeShiftPositiveInt64) {
+    ShiftTest myShift;
+    int64 number = 123456;
+    ASSERT_TRUE(myShift.TestMathematicRightSafeShift(number));
+}
+
+TEST(ShiftGTest,TestMathematicRightSafeShiftNegativeInt64) {
+    ShiftTest myShift;
+    int64 number = -123456;
+    ASSERT_TRUE(myShift.TestMathematicRightSafeShift(number));
+}
+
+TEST(ShiftGTest,TestMathematicLeftSafeShiftNegativeInt8) {
+    ShiftTest myShift;
+    int8 number = -56;
+    ASSERT_TRUE(myShift.TestMathematicLeftSaveShift(number));
+}
+
+TEST(ShiftGTest,TestMathematicLeftSafeShiftPositiveInt8) {
+    ShiftTest myShift;
+    int8 number = 56;
+    ASSERT_TRUE(myShift.TestMathematicLeftSaveShift(number));
+}
+
+TEST(ShiftGTest,TestMathematicLeftSafeShiftPositiveInt64) {
+    ShiftTest myShift;
+    int64 number = 52453456;
+    ASSERT_TRUE(myShift.TestMathematicLeftSaveShift(number));
+}
+
+TEST(ShiftGTest,TestMathematicLeftSafeShiftNegativeInt64) {
+    ShiftTest myShift;
+    int64 number = -52453456;
+    ASSERT_TRUE(myShift.TestMathematicLeftSaveShift(number));
+}
+
