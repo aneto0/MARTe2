c81b48feb9f071c20bdb5f393c04da06728be25a
Frank Perez Paz <frankpp123@hotmail.com>
2015-12-17 11:50:46 +0100

    #294 Modified the arrays handle to function with read, write and exception handle. Create two different arrays by each one. One for save socket as event and other to save all handle as handle.

--- Source/Core/FileSystem/L1Portability/Environment/Windows/Select.cpp
+++ Source/Core/FileSystem/L1Portability/Environment/Windows/Select.cpp
@@ -54,9 +54,24 @@ Select::Select() {
     readHandle.registeredHandles = new Handle[MAXIMUM_WAIT_OBJECTS];
     readHandle.selectHandles = new Handle[MAXIMUM_WAIT_OBJECTS];
     readHandle.selectedHandle = reinterpret_cast<Handle>(-1);
+
+    writeHandle.registeredHandles = new Handle[MAXIMUM_WAIT_OBJECTS];
+    writeHandle.selectHandles = new Handle[MAXIMUM_WAIT_OBJECTS];
+    writeHandle.selectedHandle = reinterpret_cast<Handle>(-1);
+
+    exceptionHandle.registeredHandles = new Handle[MAXIMUM_WAIT_OBJECTS];
+    exceptionHandle.selectHandles = new Handle[MAXIMUM_WAIT_OBJECTS];
+    exceptionHandle.selectedHandle = reinterpret_cast<Handle>(-1);
+
     for (uint32 i = 0; i < MAXIMUM_WAIT_OBJECTS; i++) {
         readHandle.registeredHandles[i] = NULL;
         readHandle.selectHandles[i] = NULL;
+
+        writeHandle.registeredHandles[i] = NULL;
+        writeHandle.selectHandles[i] = NULL;
+
+        exceptionHandle.registeredHandles[i] = NULL;
+        exceptionHandle.selectHandles[i] = NULL;
     }
     highestHandle = 0;
 }
@@ -64,14 +79,24 @@ Select::Select() {
 Select::~Select() {
     delete[] readHandle.registeredHandles;
     delete[] readHandle.selectHandles;
+
+    delete[] writeHandle.registeredHandles;
+    delete[] writeHandle.selectHandles;
+
+    delete[] exceptionHandle.registeredHandles;
+    delete[] exceptionHandle.selectHandles;
 }
 
 bool Select::AddReadHandle(const HandleI &handle) {
-    Handle h = handle.GetReadHandle();
     bool retVal = ((highestHandle + 1) < MAXIMUM_WAIT_OBJECTS);
+    Handle h = handle.GetReadHandle();
+    int32 indexMax = 0L;
     if (reinterpret_cast<int32>(h) >= 0) {
         if (retVal) {
-            for (int32 i = 0; i < highestHandle; i++) {
+            while (readHandle.selectHandles[indexMax] != NULL) {
+                indexMax++;
+            }
+            for (int32 i = 0; i < indexMax; i++) {
                 if (readHandle.registeredHandles[i] == h) {
                     REPORT_ERROR(ErrorManagement::FatalError, "Select::AddHandle(), Same Handle.");
                     retVal = false;
@@ -85,12 +110,12 @@ bool Select::AddReadHandle(const HandleI &handle) {
                 int32 hSocket = (int32) h;
                 int32 error = WSAEventSelect(hSocket, wsaevent, FD_READ);
                 if (error != SOCKET_ERROR) {
-                    readHandle.registeredHandles[highestHandle] = wsaevent;
+                    readHandle.registeredHandles[indexMax] = wsaevent;
                 }
                 else {
-                    readHandle.registeredHandles[highestHandle] = h;
+                    readHandle.registeredHandles[indexMax] = h;
                 }
-                readHandle.selectHandles[highestHandle] = h;
+                readHandle.selectHandles[indexMax] = h;
             }
 
             highestHandle++;
@@ -104,12 +129,16 @@ bool Select::AddReadHandle(const HandleI &handle) {
 }
 
 bool Select::AddWriteHandle(const HandleI &handle) {
-    Handle h = handle.GetWriteHandle();
     bool retVal = ((highestHandle + 1) < MAXIMUM_WAIT_OBJECTS);
+    Handle h = handle.GetWriteHandle();
+    int32 indexMax = 0L;
     if (reinterpret_cast<int32>(h) >= 0) {
         if (retVal) {
-            for (int32 i = 0; i < highestHandle; i++) {
-                if (readHandle.registeredHandles[i] == h) {
+            while (writeHandle.selectHandles[indexMax] != NULL) {
+                indexMax++;
+            }
+            for (int32 i = 0; i < indexMax; i++) {
+                if (writeHandle.registeredHandles[i] == h) {
                     REPORT_ERROR(ErrorManagement::FatalError, "Select::AddHandle(), Same Handle.");
                     retVal = false;
                 }
@@ -122,12 +151,12 @@ bool Select::AddWriteHandle(const HandleI &handle) {
                 int32 hSocket = (int32) h;
                 int32 error = WSAEventSelect(hSocket, wsaevent, FD_READ);
                 if (error != SOCKET_ERROR) {
-                    readHandle.registeredHandles[highestHandle] = wsaevent;
+                    writeHandle.registeredHandles[indexMax] = wsaevent;
                 }
                 else {
-                    readHandle.registeredHandles[highestHandle] = h;
+                    writeHandle.registeredHandles[indexMax] = h;
                 }
-                readHandle.selectHandles[highestHandle] = h;
+                writeHandle.selectHandles[indexMax] = h;
             }
 
             highestHandle++;
@@ -141,7 +170,44 @@ bool Select::AddWriteHandle(const HandleI &handle) {
 }
 
 bool Select::AddExceptionHandle(const HandleI &handle) {
-    return AddReadHandle(handle);
+    bool retVal = ((highestHandle + 1) < MAXIMUM_WAIT_OBJECTS);
+    Handle h = handle.GetReadHandle();
+    int32 indexMax = 0L;
+    if (reinterpret_cast<int32>(h) >= 0) {
+        if (retVal) {
+            while (exceptionHandle.selectHandles[indexMax] != NULL) {
+                indexMax++;
+            }
+            for (int32 i = 0; i < indexMax; i++) {
+                if (exceptionHandle.registeredHandles[i] == h) {
+                    REPORT_ERROR(ErrorManagement::FatalError, "Select::AddHandle(), Same Handle.");
+                    retVal = false;
+                }
+            }
+        }
+        if (retVal) {
+            WSAEVENT wsaevent;
+            wsaevent = WSACreateEvent();
+            if (wsaevent != WSA_INVALID_EVENT) {
+                int32 hSocket = (int32) h;
+                int32 error = WSAEventSelect(hSocket, wsaevent, FD_READ);
+                if (error != SOCKET_ERROR) {
+                    exceptionHandle.registeredHandles[indexMax] = wsaevent;
+                }
+                else {
+                    exceptionHandle.registeredHandles[indexMax] = h;
+                }
+                exceptionHandle.selectHandles[indexMax] = h;
+            }
+
+            highestHandle++;
+        }
+    }
+    else {
+        REPORT_ERROR(ErrorManagement::FatalError, "Select::AddHandle(). Invalid descriptor.");
+        retVal = false;
+    }
+    return retVal;
 }
 
 bool Select::RemoveReadHandle(const HandleI &handle) {
@@ -173,12 +239,12 @@ bool Select::RemoveWriteHandle(const HandleI &handle) {
     Handle h = handle.GetWriteHandle();
     if (reinterpret_cast<int32>(h) >= 0) {
         for (int32 i = 0; i < (MAXIMUM_WAIT_OBJECTS - 1); i++) {
-            if (readHandle.selectHandles[i] == h) {
+            if (writeHandle.selectHandles[i] == h) {
                 retVal = true;
             }
             if (retVal) {
-                readHandle.registeredHandles[i] = readHandle.registeredHandles[i + 1];
-                readHandle.selectHandles[i] = readHandle.selectHandles[i + 1];
+                writeHandle.registeredHandles[i] = writeHandle.registeredHandles[i + 1];
+                writeHandle.selectHandles[i] = writeHandle.selectHandles[i + 1];
             }
         }
     }
@@ -193,13 +259,39 @@ bool Select::RemoveWriteHandle(const HandleI &handle) {
 }
 
 bool Select::RemoveExceptionHandle(const HandleI &handle) {
-    return RemoveReadHandle(handle);
+    bool retVal = false;
+    Handle h = handle.GetReadHandle();
+    if (reinterpret_cast<int32>(h) >= 0) {
+        for (int32 i = 0; i < (MAXIMUM_WAIT_OBJECTS - 1); i++) {
+            if (exceptionHandle.selectHandles[i] == h) {
+                retVal = true;
+            }
+            if (retVal) {
+                exceptionHandle.registeredHandles[i] = exceptionHandle.registeredHandles[i + 1];
+                exceptionHandle.selectHandles[i] = exceptionHandle.selectHandles[i + 1];
+            }
+        }
+    }
+    else {
+        REPORT_ERROR(ErrorManagement::FatalError, "Select::AddHandle(). Invalid descriptor.");
+        retVal = false;
+    }
+    if (retVal) {
+        highestHandle--;
+    }
+    return retVal;
 }
 
 void Select::ClearAllHandles() {
     for (int32 i = 0; i < highestHandle; i++) {
         readHandle.registeredHandles[i] = NULL;
         readHandle.selectHandles[i] = NULL;
+
+        writeHandle.registeredHandles[i] = NULL;
+        writeHandle.selectHandles[i] = NULL;
+
+        exceptionHandle.registeredHandles[i] = NULL;
+        exceptionHandle.selectHandles[i] = NULL;
     }
     highestHandle = 0;
     return;
@@ -216,13 +308,47 @@ bool Select::IsSet(const HandleI &handle) const {
         else if (readHandle.selectedHandle == hw) {
             retVal = true;
         }
+
+        if (writeHandle.selectedHandle == hr) {
+            retVal = true;
+        }
+        else if (writeHandle.selectedHandle == hw) {
+            retVal = true;
+        }
+
+        if (exceptionHandle.selectedHandle == hr) {
+            retVal = true;
+        }
+        else if (exceptionHandle.selectedHandle == hw) {
+            retVal = true;
+        }
     }
     return retVal;
 }
 
 int32 Select::WaitUntil(const TimeoutType &msecTimeout) {
+    Handle * allHandles = new Handle[MAXIMUM_WAIT_OBJECTS];
+    uint32 i = 0;
+    uint32 aux = 0;
+    while (readHandle.registeredHandles[i] != NULL) {
+        allHandles[aux] = readHandle.registeredHandles[i];
+        i++;
+        aux++;
+    }
+    i = 0;
+    while (writeHandle.registeredHandles[i] != NULL) {
+        allHandles[aux] = writeHandle.registeredHandles[i];
+        i++;
+        aux++;
+    }
+    i = 0;
+    while (exceptionHandle.registeredHandles[i] != NULL) {
+        allHandles[aux] = exceptionHandle.registeredHandles[i];
+        i++;
+        aux++;
+    }
 
-    int32 ret = WaitForMultipleObjectsEx(static_cast<DWORD>(highestHandle), &readHandle.registeredHandles[0], false, msecTimeout.GetTimeoutMSec(), true);
+    int32 ret = WaitForMultipleObjectsEx(static_cast<DWORD>(highestHandle), &allHandles[0], false, msecTimeout.GetTimeoutMSec(), true);
     if (ret == 0x102l) {
         ret = 0;
     }
