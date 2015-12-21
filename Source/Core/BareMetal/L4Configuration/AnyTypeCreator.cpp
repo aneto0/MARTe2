975c32e8b67d6e0c0bdf4e258904b5bcffba97c3
gferro <Giuseppe.Ferro@uniroma2.it>
2015-12-21 10:13:32 +0100

    Refresh in progress

---
+++ Source/Core/BareMetal/L4Configuration/AnyTypeCreator.cpp
@@ -0,0 +1,276 @@
+/**
+ * @file AnyTypeCreator.cpp
+ * @brief Source file for class AnyTypeCreator
+ * @date 07/12/2015
+ * @author Giuseppe Ferrò
+ *
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This source file contains the definition of all the methods for
+ * the class AnyTypeCreator (public, protected, and private). Be aware that some 
+ * methods, such as those inline could be defined on the header file, instead.
+ */
+
+#define DLL_API
+
+/*---------------------------------------------------------------------------*/
+/*                         Standard header includes                          */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                         Project header includes                           */
+/*---------------------------------------------------------------------------*/
+
+#include "AnyTypeCreator.h"
+#include "TypeConversion.h"
+#include "StringHelper.h"
+
+/*---------------------------------------------------------------------------*/
+/*                           Static definitions                              */
+/*---------------------------------------------------------------------------*/
+
+namespace MARTe {
+
+/*lint -e9150 [MISRA C++ Rule 11-0-1]. Justification: Used a structure instead of a class. */
+struct TypeCastInfo {
+    TypeDescriptor typeDes;
+    const char8 *castName;
+};
+
+static const TypeCastInfo castTypes[] = { { CharString, "string" }, { SignedInteger8Bit, "int8" }, { SignedInteger16Bit, "int16" }, { SignedInteger32Bit,
+        "int32" }, { SignedInteger64Bit, "int64" }, { UnsignedInteger8Bit, "uint8" }, { UnsignedInteger16Bit, "uint16" }, { UnsignedInteger32Bit, "uint32" }, {
+        UnsignedInteger64Bit, "uint64" }, { Float32Bit, "float32" }, { Float64Bit, "float64" }, { CharString, static_cast<const char8*>(NULL)}};
+}
+
+/*---------------------------------------------------------------------------*/
+/*                           Method definitions                              */
+/*---------------------------------------------------------------------------*/
+
+namespace MARTe {
+
+AnyTypeCreator::AnyTypeCreator(const uint32 granularityIn) {
+    typeIndex = 0u;
+    memory = static_cast<StaticListHolder *>(NULL);
+    granularity = granularityIn;
+}
+
+/*lint -e{1579} .Justification: The destructor calls an external function. */
+/*lint -e{1551} Justification: Memory has to be freed in the destructor.
+ * No exceptions should be thrown given that the memory is managed exclusively managed by this class.". */
+AnyTypeCreator::~AnyTypeCreator() {
+    CleanUp(0u);
+}
+
+void AnyTypeCreator::CleanUp(const uint32 granularityIn) {
+    if (memory != NULL) {
+        // in this case delete the string on heap
+        if (memory->GetSize() > 0u) {
+            if (castTypes[typeIndex].typeDes == CharString) {
+                char8* stringElement = reinterpret_cast<char8 **>(memory->GetAllocatedMemory())[0];
+                if (!HeapManager::Free(reinterpret_cast<void* &>(stringElement))) {
+                    REPORT_ERROR(ErrorManagement::FatalError, "ReadMatrix: Failed HeapManager::Free()");
+                }
+            }
+        }
+        delete memory;
+        typeIndex = 0u;
+        memory = static_cast<StaticListHolder *>(NULL);
+    }
+    granularity=granularityIn;
+}
+
+AnyType AnyTypeCreator::Create(const uint8 nOfDimensions,
+                               const uint32 dimensionSize[3]) const {
+
+    AnyType element;
+    bool ret = (memory != NULL);
+    if (ret) {
+        /*lint -e{613} .Justification: possible NULL memory is checked before entering here*/
+        ret = (((dimensionSize[0] * dimensionSize[1]) * dimensionSize[2]) == (memory->GetSize()));
+        if (ret) {
+            uint32 dimCheck = 0u;
+            for (uint32 i = 0u; i < 3u; i++) {
+                if (dimensionSize[i] > 1u) {
+                    dimCheck = (i + 1u);
+                }
+            }
+            ret = (nOfDimensions >= dimCheck) && (nOfDimensions < 3u);
+            if (ret) {
+
+                if ((castTypes[typeIndex].typeDes.type == CCString) && (nOfDimensions == 0u)) {
+                    /*lint -e{613} .Justification: possible NULL memory is checked before entering here*/
+                    element = AnyType(castTypes[typeIndex].typeDes, static_cast<uint8>(0u), *static_cast<char8**>(memory->GetAllocatedMemory()));
+                }
+                else {
+                    /*lint -e{613} .Justification: possible NULL memory is checked before entering here*/
+                    element = AnyType(castTypes[typeIndex].typeDes, static_cast<uint8>(0u), memory->GetAllocatedMemory());
+                }
+                element.SetNumberOfDimensions(nOfDimensions);
+                for (uint32 i = 0u; i < 3u; i++) {
+                    element.SetNumberOfElements(i, dimensionSize[i]);
+                }
+            }
+        }
+    }
+
+    return element;
+}
+
+bool AnyTypeCreator::Add(const char8 * const type,
+                         const char8 * const data) {
+
+    bool ret = false;
+
+    // try to insert an element with a different size
+    if (memory != NULL) {
+        if(StringHelper::Compare(type, castTypes[typeIndex].castName) != 0) {
+            REPORT_ERROR(ErrorManagement::FatalError, "ToType: Type mismatch!");
+        }
+    }
+    else {
+        typeIndex = 0u;
+        while ((castTypes[typeIndex].castName != NULL) && (!ret)) {
+            if (StringHelper::Compare(type, castTypes[typeIndex].castName) == 0) {
+                ret = true;
+            }
+            else {
+                typeIndex++;
+            }
+        }
+    }
+
+    if (!ret) {
+        REPORT_ERROR(ErrorManagement::Warning, "ToType: Type not found; automatic cast to C-string");
+    }
+
+    ret = false;
+    if (castTypes[typeIndex].typeDes == CharString) {
+        if (memory == NULL) {
+            memory = new StaticListHolder(static_cast<uint32>(sizeof(char8 *)), granularity);
+        }
+        uint32 size = StringHelper::Length(data) + 1u;
+        char8 *cString = static_cast<char8 *>(HeapManager::Malloc(size));
+        if (StringHelper::Copy(cString, data)) {
+            ret = memory->Add(reinterpret_cast<void*>(&cString));
+        }
+    }
+    else if (castTypes[typeIndex].typeDes == SignedInteger8Bit) {
+        if (memory == NULL) {
+            memory = new StaticListHolder(static_cast<uint32>(sizeof(int8)), granularity);
+        }
+        int8 possibleInt8 = 0;
+        if (TypeConvert(possibleInt8, data)) {
+            ret = memory->Add(reinterpret_cast<void*>(&possibleInt8));
+        }
+    }
+    else if (castTypes[typeIndex].typeDes == SignedInteger16Bit) {
+        if (memory == NULL) {
+            memory = new StaticListHolder(static_cast<uint32>(sizeof(int16)), granularity);
+        }
+        int16 possibleInt16 = 0;
+        if (TypeConvert(possibleInt16, data)) {
+            ret = memory->Add(reinterpret_cast<void*>(&possibleInt16));
+        }
+    }
+    else if (castTypes[typeIndex].typeDes == SignedInteger32Bit) {
+        if (memory == NULL) {
+            memory = new StaticListHolder(static_cast<uint32>(sizeof(int32)), granularity);
+        }
+        int32 possibleInt32 = 0;
+        if (TypeConvert(possibleInt32, data)) {
+            ret = memory->Add(reinterpret_cast<void*>(&possibleInt32));
+        }
+    }
+    else if (castTypes[typeIndex].typeDes == SignedInteger64Bit) {
+        if (memory == NULL) {
+            memory = new StaticListHolder(static_cast<uint32>(sizeof(int64)), granularity);
+        }
+        int64 possibleInt64 = 0;
+        if (TypeConvert(possibleInt64, data)) {
+            ret = memory->Add(reinterpret_cast<void*>(&possibleInt64));
+        }
+    }
+    else if (castTypes[typeIndex].typeDes == UnsignedInteger8Bit) {
+        if (memory == NULL) {
+            memory = new StaticListHolder(static_cast<uint32>(sizeof(uint8)), granularity);
+        }
+        uint8 possibleUInt8 = 0u;
+        if (TypeConvert(possibleUInt8, data)) {
+            ret = memory->Add(reinterpret_cast<void*>(&possibleUInt8));
+        }
+    }
+    else if (castTypes[typeIndex].typeDes == UnsignedInteger16Bit) {
+        if (memory == NULL) {
+            memory = new StaticListHolder(static_cast<uint32>(sizeof(uint16)), granularity);
+        }
+        uint16 possibleUInt16 = 0u;
+        if (TypeConvert(possibleUInt16, data)) {
+            ret = memory->Add(reinterpret_cast<void*>(&possibleUInt16));
+        }
+    }
+    else if (castTypes[typeIndex].typeDes == UnsignedInteger32Bit) {
+        if (memory == NULL) {
+            memory = new StaticListHolder(static_cast<uint32>(sizeof(uint32)), granularity);
+        }
+        uint32 possibleUInt32 = 0u;
+        if (TypeConvert(possibleUInt32, data)) {
+            ret = memory->Add(reinterpret_cast<void*>(&possibleUInt32));
+        }
+    }
+    else if (castTypes[typeIndex].typeDes == UnsignedInteger64Bit) {
+        if (memory == NULL) {
+            memory = new StaticListHolder(static_cast<uint32>(sizeof(uint64)), granularity);
+        }
+        uint64 possibleUInt64 = 0u;
+        if (TypeConvert(possibleUInt64, data)) {
+            ret = memory->Add(reinterpret_cast<void*>(&possibleUInt64));
+        }
+    }
+    else if (castTypes[typeIndex].typeDes == Float32Bit) {
+        if (memory == NULL) {
+            memory = new StaticListHolder(static_cast<uint32>(sizeof(float32)), granularity);
+        }
+        float32 possibleFloat32 = 0.0F;
+        if (TypeConvert(possibleFloat32, data)) {
+            ret = memory->Add(reinterpret_cast<void*>(&possibleFloat32));
+        }
+    }
+    else if (castTypes[typeIndex].typeDes == Float64Bit) {
+        if (memory == NULL) {
+            memory = new StaticListHolder(static_cast<uint32>(sizeof(float64)), granularity);
+        }
+        float64 possibleFloat64 = 0.0;
+        if (TypeConvert(possibleFloat64, data)) {
+            ret = memory->Add(reinterpret_cast<void*>(&possibleFloat64));
+        }
+    }
+    else {
+        REPORT_ERROR(ErrorManagement::FatalError, "ToType: Invalid type in input!");
+    }
+
+    if (!ret) {
+        REPORT_ERROR(ErrorManagement::FatalError, "ToType: Failed conversion to the specified type!");
+    }
+
+    return ret;
+}
+
+uint32 AnyTypeCreator::GetSize() const {
+    return (memory == NULL)?(0u):(memory->GetSize());
+}
+
+uint32 AnyTypeCreator::GetGranularity() const {
+    return granularity;
+}
+
+}
---
+++ Source/Core/BareMetal/L4Configuration/StandardParser.cpp
@@ -0,0 +1,152 @@
+/**
+ * @file StandardParser.cpp
+ * @brief Source file for class StandardParser
+ * @date 09/12/2015
+ * @author Giuseppe Ferrò
+ *
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This source file contains the definition of all the methods for
+ * the class StandardParser (public, protected, and private). Be aware that some 
+ * methods, such as those inline could be defined on the header file, instead.
+ */
+
+#define DLL_API
+
+/*---------------------------------------------------------------------------*/
+/*                         Standard header includes                          */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                         Project header includes                           */
+/*---------------------------------------------------------------------------*/
+
+#include "StandardParser.h"
+
+/*---------------------------------------------------------------------------*/
+/*                           Static definitions                              */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                           Method definitions                              */
+/*---------------------------------------------------------------------------*/
+
+namespace MARTe {
+
+static uint32 Production[] = { 0u
+
+, 3u, 9u, 10u, 22u, 3u, 10u, 11u, 18u, 6u, 11u, 23u, 1u, 2u, 12u, 24u, 10u, 11u, 23u, 1u, 2u, 3u, 25u, 1u, 4u, 12u, 24u, 5u, 11u, 26u, 1u, 2u, 16u, 2u, 12u, 13u, 2u, 12u, 14u, 2u, 12u, 15u, 3u, 13u, 27u,
+        17u, 5u, 14u, 5u, 13u, 19u, 6u, 5u, 15u, 5u, 14u, 20u, 6u, 5u, 16u, 5u, 11u, 21u, 6u, 2u, 17u, 1u, 2u, 17u, 7u, 3u, 18u, 11u, 18u, 1u, 18u, 3u, 19u, 13u, 19u, 2u, 19u, 28u, 3u, 20u, 14u, 20u, 2u,
+        20u, 29u, 3u, 21u, 11u, 21u, 2u, 21u, 30u
+        , 0u };
+
+static uint32 Production_row[] = { 0u
+
+, 1u, 5u, 9u, 16u, 27u, 33u, 36u, 39u, 42u, 46u, 52u, 58u, 64u, 67u, 70u, 74u, 76u, 80u, 83u, 87u, 90u, 94u, 0u };
+
+static uint32 ParseArray[] = {
+
+0u, 0u, 6u, 1u, 17u, 9u, 24u, 13u, 6u, 18u, 17u, 9u, 15u, 14u, 19u, 20u, 21u, 2u, 23u, 16u, 10u, 22u, 11u, 12u, 0u, 0u, 0u };
+
+static uint32 Parse_row[] = { 0u
+
+, 2u, 16u, 17u, 1u, 4u, 15u, 17u, 18u, 6u, 11u, 3u, 9u, 15u, 0u };
+
+static uint32 Conflict[] = {
+
+0u, 0u, 7u, 25u, 0u, 3u, 8u, 4u, 7u, 26u, 27u, 3u, 3u, 5u, 3u, 0u, 3u, 3u, 3u };
+
+static uint32 Conflict_row[] = { 0u
+
+, 1u, 1u, 4u, 9u, 11u, 0u };
+
+
+static const uint32 Constants[] = { 9u, 8u, 0u, 23u, 28u, 22u, 31u, 5u };
+
+
+static const char8 * Terminal_name[] = { "0"
+
+, "STRING", "=", "(", ")", "{", "}", "NUMBER", "END_OF_SLK_INPUT" };
+
+
+static const char8 *GetTerminalName(const uint32 symbol) {
+    return Terminal_name[symbol];
+}
+
+StandardParser::StandardParser(StreamI &stream,
+                               StructuredDataI &databaseIn,
+                               BufferedStreamI * const err) :
+        ParserI(stream, databaseIn, err, StandardGrammar) {
+    Action[0] = static_cast<void (StandardParser::*)(void)>(NULL);
+    Action[1] = &StandardParser::End;
+    Action[2] = &StandardParser::GetNodeName;
+    Action[3] = &StandardParser::AddLeaf;
+    Action[4] = &StandardParser::GetTypeCast;
+    Action[5] = &StandardParser::CreateNode;
+    Action[6] = &StandardParser::AddScalar;
+    Action[7] = &StandardParser::EndVector;
+    Action[8] = &StandardParser::EndMatrix;
+    Action[9] = &StandardParser::BlockEnd;
+}
+
+StandardParser::~StandardParser() {
+
+}
+
+
+void StandardParser::Execute(const uint32 number) {
+    (this->*Action[number])();
+}
+
+const char8 *StandardParser::GetSymbolName(const uint32 symbol)const  {
+    const char8 *symbolName = static_cast<const char8 *>(NULL);
+
+    if((symbol > 0u) && (symbol < Constants[ParserConstant::START_SYMBOL])) {
+        symbolName=GetTerminalName(symbol);
+    }
+    else {
+        symbolName="not a symbol";
+    }
+    return symbolName;
+}
+
+uint32 &StandardParser::GetProduction(const uint32 index)const  {
+    return Production[index];
+}
+
+uint32 StandardParser::GetProductionRow(const uint32 index)const  {
+    return Production_row[index];
+}
+
+uint32 StandardParser::GetParse(const uint32 index)const  {
+    return ParseArray[index];
+}
+
+uint32 StandardParser::GetParseRow(const uint32 index)const  {
+    return Parse_row[index];
+}
+
+uint32 StandardParser::GetConflict(const uint32 index)const  {
+    return Conflict[index];
+}
+
+uint32 StandardParser::GetConflictRow(const uint32 index)const  {
+    return Conflict_row[index];
+}
+
+uint32 StandardParser::GetConstant(const uint32 index)const  {
+    return Constants[index];
+}
+
+
+}
