b97157e1886198776b5755b4f244c3840686cc2f
gferro <Giuseppe.Ferro@uniroma2.it>
2016-01-18 16:30:33 +0100

    #188 Added ZeroTerminatedArray

---
+++ Source/Core/BareMetal/L0Types/ZeroTerminatedArray.h
@@ -0,0 +1,131 @@
+/**
+ * @file ZeroTerminatedArray.h
+ * @brief Header file for class ZeroTerminatedArray
+ * @date 22/09/2015
+ * @author Filippo Sartori
+ *
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This header file contains the declaration of the class ZeroTerminatedArray
+ * with all of its public, protected and private members. It may also include
+ * definitions for inline methods which need to be visible to the compiler.
+ */
+
+#ifndef ZEROTERMINATEDARRAY_H_
+#define ZEROTERMINATEDARRAY_H_
+
+/*---------------------------------------------------------------------------*/
+/*                        Standard header includes                           */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                        Project header includes                            */
+/*---------------------------------------------------------------------------*/
+
+#include "CompilerTypes.h"
+
+/*---------------------------------------------------------------------------*/
+/*                           Class declaration                               */
+/*---------------------------------------------------------------------------*/
+
+namespace MARTe {
+
+template<typename T>
+class ZeroTerminatedArray {
+public:
+
+    /**
+     * @brief Default constructor.
+     * @param[in] arrayIn is the zero terminated array input.
+     * @pre
+     *   arrayIn is zero terminated.
+     */
+    ZeroTerminatedArray(const T *arrayIn);
+
+    /**
+     * @brief Returns the element in the specified position.
+     * @param[in] index is the element position in the array.
+     * @return the element in the \a index position.
+     */
+    inline T &operator[](uint32 index) const;
+
+    /**
+     * @brief Retrieves the size of the array.
+     * @return the number of elements in the array.
+     */
+    uint32 GetSize() const;
+
+    /**
+     * @brief Returns the pointer to the beginning of the array.
+     * @return the pointer to the beginning of the array.
+     */
+    const T * GetList();
+private:
+
+    /**
+     * @brief Checks if the input is the array terminal.
+     * @details This function must be specialized for each new template type. The default
+     * terminal is static_cast<T>(0)
+     * @param[in] data is the element to be checked.
+     * @return true if \a data is the array terminal, false otherwise.
+     */
+    bool Zero(const T & data) const;
+
+    T * array;
+
+};
+
+/*---------------------------------------------------------------------------*/
+/*                        Inline method definitions                          */
+/*---------------------------------------------------------------------------*/
+
+template<typename T>
+ZeroTerminatedArray<T>::ZeroTerminatedArray(const T *arrayIn) :
+        array(arrayIn) {
+}
+
+/**
+ * Does not control limit
+ * */
+template<typename T>
+inline T &ZeroTerminatedArray<T>::operator[](uint32 index) const {
+    return array[index];
+}
+
+template<typename T>
+uint32 ZeroTerminatedArray<T>::GetSize() const {
+    uint32 size = 0u;
+    if (array != NULL_PTR(T*)) {
+        const T * listP = array;
+        while (!Zero(listP[0])) {
+            listP = &listP[1];
+            size++;
+        }
+    }
+    return size;
+}
+
+template<typename T>
+const T * ZeroTerminatedArray<T>::GetList() {
+    return array;
+}
+
+template<typename T>
+bool ZeroTerminatedArray<T>::Zero(const T & data) const {
+    return (data == static_cast<T>(0));
+}
+
+}
+
+#endif /* ZEROTERMINATEDARRAY_H_ */
+
