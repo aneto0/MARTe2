50db9809c25d142ae1cfdbccdbb7c2ccdb3a000b
Ivan Herrero Molina <ivan.herrero@gtd.eu>
2016-05-12 18:12:29 +0200

    #305 - Initial version of Test/Core/BareMetal/L4Messages.

---
+++ Test/Core/BareMetal/L4Messages/MessageITest.h
@@ -0,0 +1,57 @@
+/**
+ * @file MessageITest.h
+ * @brief Header file for class MessageITest
+ * @date 11/05/2016
+ * @author Ivan Herrero
+ *
+ * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
+ * the Development of Fusion Energy ('Fusion for Energy').
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
+ * by the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+ *
+ * @warning Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Licence is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the Licence permissions and limitations under the Licence.
+
+ * @details This header file contains the declaration of the class MessageITest
+ * with all of its public, protected and private members. It may also include
+ * definitions for inline methods which need to be visible to the compiler.
+ */
+
+#ifndef MESSAGEITEST_H_
+#define MESSAGEITEST_H_
+
+/*---------------------------------------------------------------------------*/
+/*                        Standard header includes                           */
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+/*                        Project header includes                            */
+/*---------------------------------------------------------------------------*/
+#include "MessageITest.h"
+/*---------------------------------------------------------------------------*/
+/*                           Class declaration                               */
+/*---------------------------------------------------------------------------*/
+
+/**
+ * @brief Tests all the TokenInfo functions
+ */
+class MessageITest {
+public:
+
+    /**
+     * @brief Tests the default constructor.
+     */
+    bool TestDefaultConstructor();
+
+};
+
+/*---------------------------------------------------------------------------*/
+/*                        Inline method definitions                          */
+/*---------------------------------------------------------------------------*/
+
+#endif /* MESSAGEITEST_H_ */
+
