a210ac5aebb7db847b633b0762b57aa8d51bec8b
Ivan Herrero Molina <ivan.herrero@gtd.eu>
2016-08-01 18:53:40 +0200

    #305 - Fix segmentation fault when testing CallRegisteredMethodLauncher::GetResults() and CallRegisteredMethodLauncherT<>::GetResults().

--- Test/Core/BareMetal/L2Objects/CallRegisteredMethodLauncherTTest.cpp
+++ Test/Core/BareMetal/L2Objects/CallRegisteredMethodLauncherTTest.cpp
@@ -45,6 +45,12 @@ namespace {
  */
 class ClassWithCallableMethods2: public ClassWithCallableMethods {};
 
+/**
+ * @brief This class is a fresh version of ClassWithCallableMethods, providing
+ * the same methods but without automatic registering.
+ */
+class ClassWithCallableMethods3: public ClassWithCallableMethods {};
+
 }
 
 /*---------------------------------------------------------------------------*/
@@ -69,19 +75,19 @@ bool CallRegisteredMethodLauncherTTest::TestDefaultConstructor() {
 }
 
 bool CallRegisteredMethodLauncherTTest::TestTest() {
+
+    /*
+     * Warning: When registering methods with a cast like:
+     *   (bool (ClassWithCallableMethods::*)())&ClassWithCallableMethods2::OverloadedMethod
+     * If the method does not exist in the derived class, the cast must be
+     * referred to the base class, while the method must be to the derived one.
+     */
+
     using namespace MARTe;
     bool result = true;
     ClassRegistryItem* const cri = ClassRegistryItemT<ClassWithCallableMethods2>::Instance();
     ClassMethodInterfaceMapper cmim[] = { &ClassWithCallableMethods2::MethodWithInputInteger, &ClassWithCallableMethods2::MethodWithOutputInteger, &ClassWithCallableMethods2::MethodWithInputOutputInteger, &ClassWithCallableMethods2::FaultyMethod, &ClassWithCallableMethods2::MethodWithInputReferenceContainer, &ClassWithCallableMethods2::MethodWithOutputReferenceContainer, &ClassWithCallableMethods2::MethodWithInputOutputReferenceContainer, &ClassWithCallableMethods2::MethodWithInputIntegerByCopy, &ClassWithCallableMethods2::MethodWithInputReferenceContainerByCopy, (bool (ClassWithCallableMethods::*)())&ClassWithCallableMethods2::OverloadedMethod, (bool (ClassWithCallableMethods::*)(int&))&ClassWithCallableMethods2::OverloadedMethod, (bool (ClassWithCallableMethods::*)(MARTe::ReferenceContainer&))&ClassWithCallableMethods2::OverloadedMethod };
     const char* names = "&ClassWithCallableMethods2::MethodWithInputInteger, &ClassWithCallableMethods2::MethodWithOutputInteger, &ClassWithCallableMethods2::MethodWithInputOutputInteger, &ClassWithCallableMethods2::FaultyMethod, &ClassWithCallableMethods2::MethodWithInputReferenceContainer, &ClassWithCallableMethods2::MethodWithOutputReferenceContainer, &ClassWithCallableMethods2::MethodWithInputOutputReferenceContainer, &ClassWithCallableMethods2::MethodWithInputIntegerByCopy, &ClassWithCallableMethods2::MethodWithInputReferenceContainerByCopy, (bool (ClassWithCallableMethods::*)())&ClassWithCallableMethods2::OverloadedMethod, (bool (ClassWithCallableMethods::*)(int&))&ClassWithCallableMethods2::OverloadedMethod, (bool (ClassWithCallableMethods::*)(MARTe::ReferenceContainer&))&ClassWithCallableMethods2::OverloadedMethod";
-
-    /*
-     * Warning about the registered methods with a cast like:
-     *
-     *   (bool (ClassWithCallableMethods::*)())&ClassWithCallableMethods2::OverloadedMethod
-     *
-     * If the method does not exist in the derived class, the cast must be referred to the base class, while the method must be to the derived one.
-     */
 
     ClassMethodsRegistryItem cmri(cri, cmim, names);
     {
@@ -229,13 +235,169 @@ bool CallRegisteredMethodLauncherTTest::TestTest() {
 }
 
 bool CallRegisteredMethodLauncherTTest::TestGetResults() {
-    bool result;
+
     /*
      * The method GetResults() work in tandem with Test(), in fact the
      * execution of GetResults() is linked to the last execution of Test(),
-     * so it is not worth it to create a different unit test. Therefore,
-     * we call TestTest() again.
+     * so the test for GetResults is the same that the Test's one, but using
+     * a new ClassWithCallableMethods named ClassWithCallableMethods3.
+     *
+     * Warning: Trying to recycle ClassWithCallableMethods2, i.e. calling
+     * TestTest() again, will cause a segmentation fault.
+     *
+     * Warning: When registering methods with a cast like:
+     *   (bool (ClassWithCallableMethods::*)())&ClassWithCallableMethods3::OverloadedMethod
+     * If the method does not exist in the derived class, the cast must be
+     * referred to the base class, while the method must be to the derived one.
      */
-    result = TestTest();
+
+    using namespace MARTe;
+    bool result = true;
+    ClassRegistryItem* const cri = ClassRegistryItemT<ClassWithCallableMethods3>::Instance();
+    ClassMethodInterfaceMapper cmim[] = { &ClassWithCallableMethods3::MethodWithInputInteger, &ClassWithCallableMethods3::MethodWithOutputInteger, &ClassWithCallableMethods3::MethodWithInputOutputInteger, &ClassWithCallableMethods3::FaultyMethod, &ClassWithCallableMethods3::MethodWithInputReferenceContainer, &ClassWithCallableMethods3::MethodWithOutputReferenceContainer, &ClassWithCallableMethods3::MethodWithInputOutputReferenceContainer, &ClassWithCallableMethods3::MethodWithInputIntegerByCopy, &ClassWithCallableMethods3::MethodWithInputReferenceContainerByCopy, (bool (ClassWithCallableMethods::*)())&ClassWithCallableMethods3::OverloadedMethod, (bool (ClassWithCallableMethods::*)(int&))&ClassWithCallableMethods3::OverloadedMethod, (bool (ClassWithCallableMethods::*)(MARTe::ReferenceContainer&))&ClassWithCallableMethods3::OverloadedMethod };
+    const char* names = "&ClassWithCallableMethods3::MethodWithInputInteger, &ClassWithCallableMethods3::MethodWithOutputInteger, &ClassWithCallableMethods3::MethodWithInputOutputInteger, &ClassWithCallableMethods3::FaultyMethod, &ClassWithCallableMethods3::MethodWithInputReferenceContainer, &ClassWithCallableMethods3::MethodWithOutputReferenceContainer, &ClassWithCallableMethods3::MethodWithInputOutputReferenceContainer, &ClassWithCallableMethods3::MethodWithInputIntegerByCopy, &ClassWithCallableMethods3::MethodWithInputReferenceContainerByCopy, (bool (ClassWithCallableMethods::*)())&ClassWithCallableMethods3::OverloadedMethod, (bool (ClassWithCallableMethods::*)(int&))&ClassWithCallableMethods3::OverloadedMethod, (bool (ClassWithCallableMethods::*)(MARTe::ReferenceContainer&))&ClassWithCallableMethods3::OverloadedMethod";
+
+    ClassMethodsRegistryItem cmri(cri, cmim, names);
+    {
+        bool status;
+        ClassWithCallableMethods3 context;
+        ReferenceContainer params;
+        CallRegisteredMethodLauncherT<ReferenceContainer&> target(&context, "NonRegisteredMethod", params);
+        status = target.Test(&cmri);
+        result &= (!status && target.GetResults().unsupportedFeature);
+    }
+    {
+        bool status;
+        ClassWithCallableMethods3 context;
+        ReferenceContainer params;
+        CallRegisteredMethodLauncherT<ReferenceContainer&> target(&context, "FaultyMethod", params);
+        status = target.Test(&cmri);
+        result &= (status && target.GetResults().functionError);
+        result &= (context.GetLastMethodExecuted() == "FaultyMethod(MARTe::ReferenceContainer&)");
+    }
+    {
+        bool status;
+        ClassWithCallableMethods3 context;
+        int params = 0;
+        CallRegisteredMethodLauncherT<int&> target(&context, "OverloadedMethod", params);
+        status = target.Test(&cmri);
+        result &= (status && !target.GetResults().unsupportedFeature);
+        result &= (context.GetLastMethodExecuted() == "OverloadedMethod(int&)");
+    }
+    {
+        bool status;
+        ClassWithCallableMethods3 context;
+        ReferenceContainer params;
+        CallRegisteredMethodLauncherT<ReferenceContainer&> target(&context, "OverloadedMethod", params);
+        status = target.Test(&cmri);
+        result &= (status && !target.GetResults().unsupportedFeature);
+        result &= (context.GetLastMethodExecuted() == "OverloadedMethod(MARTe::ReferenceContainer&)");
+    }
+    {
+        bool status;
+        ClassWithCallableMethods3 context;
+        int params = 10;
+        CallRegisteredMethodLauncherT<int&> target(&context, "MethodWithInputInteger", params);
+        status = target.Test(&cmri);
+        result &= (status && !target.GetResults().unsupportedFeature);
+        result &= (context.GetLastMethodExecuted() == "MethodWithInputInteger(int&)");
+    }
+    {
+        bool status;
+        ClassWithCallableMethods3 context;
+        int params = 0;
+        CallRegisteredMethodLauncherT<int&> target(&context, "MethodWithOutputInteger", params);
+        status = target.Test(&cmri);
+        result &= (status && !target.GetResults().unsupportedFeature);
+        result &= (params == 20);
+        result &= (context.GetLastMethodExecuted() == "MethodWithOutputInteger(int&)");
+    }
+    {
+        bool status;
+        ClassWithCallableMethods3 context;
+        int params = 30;
+        CallRegisteredMethodLauncherT<int&> target(&context, "MethodWithInputOutputInteger", params);
+        status = target.Test(&cmri);
+        result &= (status && !target.GetResults().unsupportedFeature);
+        result &= (params == (30 + 5));
+        result &= (context.GetLastMethodExecuted() == "MethodWithInputOutputInteger(int&)");
+    }
+    {
+        bool status;
+        ClassWithCallableMethods3 context;
+        ReferenceContainer params;
+        Reference obj("Object");
+        bool success;
+        success = params.Insert("TestObject", obj);
+        if (success) {
+            CallRegisteredMethodLauncherT<ReferenceContainer&> target(&context, "MethodWithInputReferenceContainer", params);
+            status = target.Test(&cmri);
+            result &= (status && !target.GetResults().unsupportedFeature);
+            result &= (context.GetLastMethodExecuted() == "MethodWithInputReferenceContainer(MARTe::ReferenceContainer&)");
+        }
+        else {
+            result = false;
+        }
+    }
+    {
+        bool status;
+        ClassWithCallableMethods3 context;
+        ReferenceContainer params;
+        Reference obj;
+        CallRegisteredMethodLauncherT<ReferenceContainer&> target(&context, "MethodWithOutputReferenceContainer", params);
+        status = target.Test(&cmri);
+        result &= (status && !target.GetResults().unsupportedFeature);
+        obj = params.Find("TestObject2");
+        result &= obj.IsValid();
+        result &= (context.GetLastMethodExecuted() == "MethodWithOutputReferenceContainer(MARTe::ReferenceContainer&)");
+    }
+    {
+        bool status;
+        ClassWithCallableMethods3 context;
+        ReferenceContainer params;
+        Reference obj("Object");
+        bool success;
+        success = params.Insert("TestObject", obj);
+        if (success) {
+            CallRegisteredMethodLauncherT<ReferenceContainer&> target(&context, "MethodWithInputOutputReferenceContainer", params);
+            status = target.Test(&cmri);
+            result &= (status && !target.GetResults().unsupportedFeature);
+            obj = params.Find("TestObject");
+            result &= !obj.IsValid();
+            obj = params.Find("TestObject2");
+            result &= obj.IsValid();
+            result &= (context.GetLastMethodExecuted() == "MethodWithInputOutputReferenceContainer(MARTe::ReferenceContainer&)");
+        }
+        else {
+            result = false;
+        }
+    }
+    {
+        bool status;
+        ClassWithCallableMethods3 context;
+        int params = 80;
+        CallRegisteredMethodLauncherT<int> target(&context, "MethodWithInputIntegerByCopy", params);
+        status = target.Test(&cmri);
+        result &= (status && !target.GetResults().unsupportedFeature);
+        result &= (context.GetLastMethodExecuted() == "MethodWithInputIntegerByCopy(int)");
+    }
+    {
+        bool status;
+        ClassWithCallableMethods3 context;
+        ReferenceContainer params;
+        Reference obj("Object");
+        bool success;
+        success = params.Insert("TestObjectIntoReferenceContainerByCopy", obj);
+        if (success) {
+            CallRegisteredMethodLauncherT<ReferenceContainer> target(&context, "MethodWithInputReferenceContainerByCopy", params);
+            status = target.Test(&cmri);
+            result &= (status && !target.GetResults().unsupportedFeature);
+            result &= (context.GetLastMethodExecuted() == "MethodWithInputReferenceContainerByCopy(MARTe::ReferenceContainer)");
+        }
+        else {
+            result = false;
+        }
+    }
+
     return result;
 }
--- Test/Core/BareMetal/L2Objects/CallRegisteredMethodLauncherTest.cpp
+++ Test/Core/BareMetal/L2Objects/CallRegisteredMethodLauncherTest.cpp
@@ -45,6 +45,12 @@ namespace {
  */
 class ClassWithCallableMethods2: public ClassWithCallableMethods {};
 
+/**
+ * @brief This class is a fresh version of ClassWithCallableMethods, providing
+ * the same methods but without automatic registering.
+ */
+class ClassWithCallableMethods3: public ClassWithCallableMethods {};
+
 }
 
 /*---------------------------------------------------------------------------*/
@@ -69,19 +75,19 @@ bool CallRegisteredMethodLauncherTest::TestDefaultConstructor() {
 }
 
 bool CallRegisteredMethodLauncherTest::TestTest() {
+
+    /*
+     * Warning: When registering methods with a cast like:
+     *   (bool (ClassWithCallableMethods::*)())&ClassWithCallableMethods2::OverloadedMethod
+     * If the method does not exist in the derived class, the cast must be
+     * referred to the base class, while the method must be to the derived one.
+     */
+
     using namespace MARTe;
     bool result = true;
     ClassRegistryItem* const cri = ClassRegistryItemT<ClassWithCallableMethods2>::Instance();
     ClassMethodInterfaceMapper cmim[] = { &ClassWithCallableMethods2::MethodWithInputInteger, &ClassWithCallableMethods2::MethodWithOutputInteger, &ClassWithCallableMethods2::MethodWithInputOutputInteger, &ClassWithCallableMethods2::FaultyMethod, &ClassWithCallableMethods2::MethodWithInputReferenceContainer, &ClassWithCallableMethods2::MethodWithOutputReferenceContainer, &ClassWithCallableMethods2::MethodWithInputOutputReferenceContainer, &ClassWithCallableMethods2::MethodWithInputIntegerByCopy, &ClassWithCallableMethods2::MethodWithInputReferenceContainerByCopy, (bool (ClassWithCallableMethods::*)())&ClassWithCallableMethods2::OverloadedMethod, (bool (ClassWithCallableMethods::*)(int&))&ClassWithCallableMethods2::OverloadedMethod, (bool (ClassWithCallableMethods::*)(MARTe::ReferenceContainer&))&ClassWithCallableMethods2::OverloadedMethod };
     const char* names = "&ClassWithCallableMethods2::MethodWithInputInteger, &ClassWithCallableMethods2::MethodWithOutputInteger, &ClassWithCallableMethods2::MethodWithInputOutputInteger, &ClassWithCallableMethods2::FaultyMethod, &ClassWithCallableMethods2::MethodWithInputReferenceContainer, &ClassWithCallableMethods2::MethodWithOutputReferenceContainer, &ClassWithCallableMethods2::MethodWithInputOutputReferenceContainer, &ClassWithCallableMethods2::MethodWithInputIntegerByCopy, &ClassWithCallableMethods2::MethodWithInputReferenceContainerByCopy, (bool (ClassWithCallableMethods::*)())&ClassWithCallableMethods2::OverloadedMethod, (bool (ClassWithCallableMethods::*)(int&))&ClassWithCallableMethods2::OverloadedMethod, (bool (ClassWithCallableMethods::*)(MARTe::ReferenceContainer&))&ClassWithCallableMethods2::OverloadedMethod";
-
-    /*
-     * Warning about the registered methods with a cast like:
-     *
-     *   (bool (ClassWithCallableMethods::*)())&ClassWithCallableMethods2::OverloadedMethod
-     *
-     * If the method does not exist in the derived class, the cast must be referred to the base class, while the method must be to the derived one.
-     */
 
     ClassMethodsRegistryItem cmri(cri, cmim, names);
     {
@@ -97,13 +103,38 @@ bool CallRegisteredMethodLauncherTest::TestTest() {
 }
 
 bool CallRegisteredMethodLauncherTest::TestGetResults() {
-    bool result;
+
     /*
      * The method GetResults() work in tandem with Test(), in fact the
      * execution of GetResults() is linked to the last execution of Test(),
-     * so it is not worth it to create a different unit test. Therefore,
-     * we call TestTest() again.
+     * so the test for GetResults is the same that the Test's one, but using
+     * a new ClassWithCallableMethods named ClassWithCallableMethods3.
+     *
+     * Warning: Trying to recycle ClassWithCallableMethods2, i.e. calling
+     * TestTest() again, will cause a segmentation fault.
+     *
+     * Warning: When registering methods with a cast like:
+     *   (bool (ClassWithCallableMethods::*)())&ClassWithCallableMethods3::OverloadedMethod
+     * If the method does not exist in the derived class, the cast must be
+     * referred to the base class, while the method must be to the derived one.
      */
-    result = TestTest();
+
+    using namespace MARTe;
+    bool result = true;
+    ClassRegistryItem* const cri = ClassRegistryItemT<ClassWithCallableMethods3>::Instance();
+    ClassMethodInterfaceMapper cmim[] = { &ClassWithCallableMethods3::MethodWithInputInteger, &ClassWithCallableMethods3::MethodWithOutputInteger, &ClassWithCallableMethods3::MethodWithInputOutputInteger, &ClassWithCallableMethods3::FaultyMethod, &ClassWithCallableMethods3::MethodWithInputReferenceContainer, &ClassWithCallableMethods3::MethodWithOutputReferenceContainer, &ClassWithCallableMethods3::MethodWithInputOutputReferenceContainer, &ClassWithCallableMethods3::MethodWithInputIntegerByCopy, &ClassWithCallableMethods3::MethodWithInputReferenceContainerByCopy, (bool (ClassWithCallableMethods::*)())&ClassWithCallableMethods3::OverloadedMethod, (bool (ClassWithCallableMethods::*)(int&))&ClassWithCallableMethods3::OverloadedMethod, (bool (ClassWithCallableMethods::*)(MARTe::ReferenceContainer&))&ClassWithCallableMethods3::OverloadedMethod };
+    const char* names = "&ClassWithCallableMethods3::MethodWithInputInteger, &ClassWithCallableMethods3::MethodWithOutputInteger, &ClassWithCallableMethods3::MethodWithInputOutputInteger, &ClassWithCallableMethods3::FaultyMethod, &ClassWithCallableMethods3::MethodWithInputReferenceContainer, &ClassWithCallableMethods3::MethodWithOutputReferenceContainer, &ClassWithCallableMethods3::MethodWithInputOutputReferenceContainer, &ClassWithCallableMethods3::MethodWithInputIntegerByCopy, &ClassWithCallableMethods3::MethodWithInputReferenceContainerByCopy, (bool (ClassWithCallableMethods::*)())&ClassWithCallableMethods3::OverloadedMethod, (bool (ClassWithCallableMethods::*)(int&))&ClassWithCallableMethods3::OverloadedMethod, (bool (ClassWithCallableMethods::*)(MARTe::ReferenceContainer&))&ClassWithCallableMethods3::OverloadedMethod";
+
+    ClassMethodsRegistryItem cmri(cri, cmim, names);
+    {
+        bool status;
+        ClassWithCallableMethods3 context;
+        CallRegisteredMethodLauncher target(&context, "OverloadedMethod");
+        status = target.Test(&cmri);
+        result &= (status && !target.GetResults().unsupportedFeature);
+        result &= (context.GetLastMethodExecuted() == "OverloadedMethod()");
+    }
+
     return result;
+
 }
