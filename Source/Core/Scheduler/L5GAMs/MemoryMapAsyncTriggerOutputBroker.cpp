5ca9522d1e48f2fbb52b499c9ed8025ba4aab613
André Neto <Andre.Neto@f4e.europa.eu>
2017-03-14 10:48:49 +0100

    Adding FlushAllTriggers function but needs merge with develop.

--- Source/Core/Scheduler/L5GAMs/MemoryMapAsyncTriggerOutputBroker.cpp
+++ Source/Core/Scheduler/L5GAMs/MemoryMapAsyncTriggerOutputBroker.cpp
@@ -373,6 +373,40 @@ ErrorManagement::ErrorType MemoryMapAsyncTriggerOutputBroker::BufferLoop(const E
     return err;
 }
 
+bool MemoryMapAsyncTriggerOutputBroker::FlushAllTriggers() {
+    uint32 i = 0u;
+    uint32 idx = writeIdx;
+    bool ret = true;
+    ret = (fastSem.FastLock() == ErrorManagement::NoError);
+    while ((i < numberOfBuffers) && (ret)) {
+        if (bufferMemoryMap != NULL_PTR(MemoryMapAsyncTriggerOutputBrokerBufferEntry *)) {
+            if (bufferMemoryMap[idx].triggered) {
+                uint32 c;
+                for (c = 0u; (c < numberOfCopies) && (ret); c++) {
+                    //Copy from the buffer to the DataSource memory
+                    if (copyTable != NULL_PTR(MemoryMapBrokerCopyTableEntry *)) {
+                        ret = MemoryOperationsHelper::Copy(copyTable[c].dataSourcePointer, bufferMemoryMap[idx].mem[c], copyTable[c].copySize);
+                    }
+                }
+                if (ret) {
+                    if (dataSourceRef.IsValid()) {
+                        //Make sure that the dataSourceRef consumes this data.
+                        ret = dataSourceRef->Synchronise();
+                    }
+                }
+                bufferMemoryMap[idx].triggered = false;
+            }
+        }
+        idx++;
+        if (idx == numberOfBuffers) {
+            idx = 0u;
+        }
+        i++;
+    }
+    fastSem.FastUnLock();
+    return ret;
+}
+
 void MemoryMapAsyncTriggerOutputBroker::ResetPreTriggerBuffers() {
     numberOfPreBuffersWritten = 0;
 }
--- Source/Core/Scheduler/L5GAMs/MemoryMapAsyncTriggerOutputBroker.h
+++ Source/Core/Scheduler/L5GAMs/MemoryMapAsyncTriggerOutputBroker.h
@@ -170,6 +170,15 @@ MemoryMapAsyncTriggerOutputBroker    ();
      */
     void ResetPreTriggerBuffers();
 
+    /**
+     * @brief Flushes all the triggers asynchronously.
+     * @details All the buffers which are marked with a trigger are copied to the DataSourceI and the Synchronise method called.
+     * Note that this happens asynchronously with respect to the buffer consumer thread, so the higher level application using this
+     *  should make sure that no data is written to this thread while this method is being called.
+     * @return true if all the Synchronise calls return true.
+     */
+    bool FlushAllTriggers();
+
 private:
 
     /**
