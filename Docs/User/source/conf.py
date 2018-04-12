7e5fcb68aa280d23dc4981bd4aae4c3bda4fe499
André Neto <Andre.Neto@f4e.europa.eu>
2018-04-12 10:40:37 +0200

    Allow wrap in tables of the rtd theme.

--- Docs/User/source/conf.py
+++ Docs/User/source/conf.py
@@ -83,6 +83,12 @@ pygments_style = 'default'
 import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# Hack to wide the table with sphinx_rtd_theme
+html_context = {
+   'css_files': [
+      '_static/theme_overrides.css',  # override wide tables in RTD theme
+   ],
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
