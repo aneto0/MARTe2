e261e48f7e508e90cc6909e8869b53a43ce903a2
André Neto <Andre.Neto@f4e.europa.eu>
2018-05-03 16:04:32 +0200

    Corrected vcisdoxygenclem pointer.

--- Docs/User/source/conf.py
+++ Docs/User/source/conf.py
@@ -209,7 +209,6 @@ extlinks = {'vciscorebml0': ('https://vcis-gitlab.f4e.europa.eu/aneto/MARTe2/blo
 			'vcisdoxygenns': ('https://vcis-jenkins.f4e.europa.eu/job/MARTe2-docs-master/doxygen/namespaceMARTe_1_1%s.html', ''),
 			'vcisdoxygenclem': ('https://vcis-jenkins.f4e.europa.eu/job/MARTe2-docs-master/doxygen/structMARTe_1_1ErrorManagement_1_1%s.html', ''),
 			'vcisdoxygenmccl': ('https://vcis-jenkins.f4e.europa.eu/job/MARTe2-Components-docs-master/doxygen/classMARTe_1_1%s.html', ''),
-			'vcisdoxygenclem': ('https://vcis-jenkins.f4e.europa.eu/job/MARTe2-docs-master/doxygen/classMARTe_1_1ErrorManagement_1_1%s.html', ''),
 			'vcisgl': ('https://vcis-gitlab.f4e.europa.eu/aneto/%s', ''),
 			'vcisgldocqa': ('https://vcis-gitlab.f4e.europa.eu/aneto/MARTe2/blob/master/Docs/QA/%s', '')
 			}
