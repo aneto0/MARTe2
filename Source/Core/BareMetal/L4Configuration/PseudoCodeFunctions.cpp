71ee9e0cd3392bbc8e137c4a2789ae1bde30a4a2
Didac <didac.magrina@gmail.com>
2020-05-05 17:25:07 +0200

    Convert FunctionRecord into a class

--- Source/Core/BareMetal/L4Configuration/PseudoCodeFunctions.cpp
+++ Source/Core/BareMetal/L4Configuration/PseudoCodeFunctions.cpp
@@ -34,6 +34,9 @@ uint32 availableFunctions = 0;
 
 FunctionRecord functionRecords[maxFunctions];
 
+FunctionRecord::FunctionRecord(CCString nameIn, uint16 numberOfInputsIn, uint16 numberOfOutputsIn, const TypeDescriptor* typesIn, Function functionIn):
+    name(nameIn), numberOfInputs(numberOfInputsIn), numberOfOutputs(numberOfOutputsIn), types(typesIn), function(functionIn)
+    {}
 
 bool FunctionRecord::TryConsume(CCString nameIn,StaticStack<TypeDescriptor,32> &typeStack, bool matchOutput,DataMemoryAddress &dataStackSize){
 	bool ret = false;
@@ -186,7 +189,7 @@ REGISTER_PCODE_FUNCTION(WRITE,int8,1,0,Write<int8>     ,SignedInteger8Bit   ,Sig
 
 #define REGISTER_CAST_FUNCTION(name,type1,type2,function)\
 	static const TypeDescriptor name ## type1 ## type2 ## _FunctionTypes[] = {Type2TypeDescriptor<type1>(), Type2TypeDescriptor<type2>()}; \
-	static const FunctionRecord name ## type1 ## type2 ## _FunctionRecord={#name,1,1,name ## type1 ## type2 ## _FunctionTypes,&function<type1,type2>}; \
+    static const FunctionRecord name ## type1 ## type2 ## _FunctionRecord(#name,1,1,name ## type1 ## type2 ## _FunctionTypes,&function<type1,type2>); \
 	static class name ## type1 ## type2 ## RegisterClass { \
 	public: name ## type1 ## type2 ## RegisterClass(){\
 			RegisterFunction(name ## type1 ## type2 ## _FunctionRecord);\
