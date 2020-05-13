adfe145d3cee7b12d6d08f37e6e386fdf847c2e8
Didac <didac.magrina@gmail.com>
2020-05-13 15:56:27 +0200

    Update PseudoCode::TestIntegration after reordering of 2 arg functions

--- Test/Core/BareMetal/L4Configuration/PseudoCodeTest.cpp
+++ Test/Core/BareMetal/L4Configuration/PseudoCodeTest.cpp
@@ -103,37 +103,37 @@ bool PseudoCodeTest::TestIntegration() {
 
     CCString expectedDebug=
             "[line]-[stackPtr]-[codePtr]::[CODE] stack-in => stack-out\n"
-            "1 - 0 - 0 :: READ  A => ((float32)1.000000)\n"
-            "2 - 1 - 2 :: READ  B => ((float32)2.000000)\n"
-            "3 - 2 - 4 :: SUB ((float32)2.000000,(float32)1.000000) => ((float32)1.000000)\n"
+            "1 - 0 - 0 :: READ  A => ((float32)2.000000)\n"
+            "2 - 1 - 2 :: READ  B => ((float32)1.000000)\n"
+            "3 - 2 - 4 :: SUB ((float32)1.000000,(float32)2.000000) => ((float32)1.000000)\n"
             "4 - 1 - 5 :: READ  float32 3.140000 => ((float32)3.140000)\n"
             "5 - 2 - 7 :: MUL ((float32)3.140000,(float32)1.000000) => ((float32)3.140000)\n"
             "6 - 1 - 8 :: DUP ((float32)3.140000) => ((float32)3.140000,(float32)3.140000)\n"
             "7 - 2 - 9 :: WRITE  C((float32)3.140000)\n"
             "8 - 1 - 11 :: READ  float32 0.500000 => ((float32)0.500000)\n"
-            "9 - 2 - 13 :: POW ((float32)0.500000,(float32)3.140000) => ((float32)0.113440)\n"
-            "10 - 1 - 14 :: TAN ((float32)0.113440) => ((float32)0.113929)\n"
-            "11 - 1 - 15 :: CAST ((float32)0.113929) => ((int32)0)\n"
-            "12 - 1 - 16 :: DUP ((int32)0) => ((int32)0,(int32)0)\n"
-            "13 - 2 - 17 :: WRITE  D((int32)0)\n"
+            "9 - 2 - 13 :: POW ((float32)0.500000,(float32)3.140000) => ((float32)1.772004)\n"
+            "10 - 1 - 14 :: TAN ((float32)1.772004) => ((float32)-4.902726)\n"
+            "11 - 1 - 15 :: CAST ((float32)-4.902726) => ((int32)-4)\n"
+            "12 - 1 - 16 :: DUP ((int32)-4) => ((int32)-4,(int32)-4)\n"
+            "13 - 2 - 17 :: WRITE  D((int32)-4)\n"
             "14 - 1 - 19 :: READ  C => ((float32)3.140000)\n"
             "15 - 2 - 21 :: LOG ((float32)3.140000) => ((float32)1.144223)\n"
             "16 - 2 - 22 :: CAST ((float32)1.144223) => ((int32)1)\n"
-            "17 - 2 - 23 :: GT ((int32)1,(int32)0) => ((uint8)1)\n"
-            "18 - 1 - 24 :: WRITE  E((uint8)1)\n"
-            "19 - 0 - 26 :: READ  N1 => ((int8)31)\n"
-            "20 - 1 - 28 :: READ  N2 => ((int8)22)\n"
-            "21 - 2 - 30 :: SUB ((int8)22,(int8)31) => ((int32)-9)\n"
+            "17 - 2 - 23 :: GT ((int32)1,(int32)-4) => ((uint8)0)\n"
+            "18 - 1 - 24 :: WRITE  E((uint8)0)\n"
+            "19 - 0 - 26 :: READ  N1 => ((int8)22)\n"
+            "20 - 1 - 28 :: READ  N2 => ((int8)31)\n"
+            "21 - 2 - 30 :: SUB ((int8)31,(int8)22) => ((int32)-9)\n"
             "22 - 1 - 31 :: DUP ((int32)-9) => ((int32)-9,(int32)-9)\n"
-            "23 - 2 - 32 :: READ  N3 => ((int8)13)\n"
-            "24 - 3 - 34 :: READ  N4 => ((int8)127)\n"
-            "25 - 4 - 36 :: DIV ((int8)127,(int8)13) => ((int8)9)\n"
+            "23 - 2 - 32 :: READ  N3 => ((int8)127)\n"
+            "24 - 3 - 34 :: READ  N4 => ((int8)13)\n"
+            "25 - 4 - 36 :: DIV ((int8)13,(int8)127) => ((int8)9)\n"
             "26 - 3 - 37 :: ADD ((int8)9,(int32)-9) => ((int32)0)\n"
             "27 - 2 - 38 :: DUP ((int32)0) => ((int32)0,(int32)0)\n"
             "28 - 3 - 39 :: WRITE  N5((int32)0)\n"
             "29 - 2 - 41 :: CAST ((int32)0) => ((int64)0)\n"
-            "30 - 3 - 42 :: GT ((int64)0,(int32)-9) => ((uint8)1)\n"
-            "31 - 1 - 43 :: WRITE  F((uint8)1)\n"
+            "30 - 3 - 42 :: GT ((int64)0,(int32)-9) => ((uint8)0)\n"
+            "31 - 1 - 43 :: WRITE  F((uint8)0)\n"
             "0 - 45 :: END\n"
     ;
     
@@ -213,27 +213,27 @@ bool PseudoCodeTest::TestIntegration() {
             index++;
             if (var->name == "A"){
                 float *x  = reinterpret_cast<float *>(&context.dataMemory[var->location]);
-                *x = 1.0;
+                *x = 2.0;
             }
             if (var->name == "B"){
                 float *x  = reinterpret_cast<float *>(&context.dataMemory[var->location]);
-                *x = 2.0;
+                *x = 1.0;
             }
             if (var->name == "N1"){
                 int8 *x  = reinterpret_cast<int8 *>(&context.dataMemory[var->location]);
-                *x = 31;
+                *x = 22;
             }
             if (var->name == "N2"){
                 int8 *x  = reinterpret_cast<int8 *>(&context.dataMemory[var->location]);
-                *x = 22;
+                *x = 31;
             }
             if (var->name == "N3"){
                 int8 *x  = reinterpret_cast<int8 *>(&context.dataMemory[var->location]);
-                *x = 13;
+                *x = 127;
             }
             if (var->name == "N4"){
                 int8 *x  = reinterpret_cast<int8 *>(&context.dataMemory[var->location]);
-                *x = 127;
+                *x = 13;
             }
         }
     }
