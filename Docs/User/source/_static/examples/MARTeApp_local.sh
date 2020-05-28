75dab10827d02c2db0c2bb49506618030c6e2d3f
f-rfx <65179125+f-rfx@users.noreply.github.com>
2020-05-28 18:45:28 +0200

    Added example configuration file

---
+++ Docs/User/source/_static/examples/MARTeApp_local.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+#Arguments -l LOADER -f FILENAME -m MESSAGE | -s STATE [-d cgdb|strace]
+#-l LOADER=The Loader to use
+#-f FILENAME=MARTe configuration file
+#-m MESSAGE=Start message
+#-s STATE=RealTimeApplication first state 
+#-d cgdb=Run with cgdb
+#-d strace=Run with strace
+
+#Run with cgdb or strace?
+DEBUG=""
+
+#Consume input arguments
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -l|--loader)
+    LOADER="$2"
+    shift # past argument
+    ;;
+    -f|--file)
+    FILE="$2"
+    shift # past argument
+    ;;
+    -m|--message)
+    MESSAGE="$2"
+    shift # past argument
+    ;;
+    -s|--state)
+    STATE="$2"
+    shift # past argument
+    ;;
+    -d|--debug)
+    DEBUG="$2"
+    shift # past argument
+    ;;    
+    --default)
+    DEFAULT=YES
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+if [ -z ${MARTe2_DIR+x} ]; then echo "Please set the MARTe2_DIR environment variable"; exit; fi
+if [ -z ${MARTe2_Components_DIR+x} ]; then echo "Please set the MARTe2_Components_DIR environment variable"; exit; fi
+
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../../../../Build/x86-linux/Examples/Core/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:Core/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_DIR/Build/x86-linux/Core/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/DataSources/EPICSCA/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/DataSources/LinuxTimer/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/DataSources/LoggerDataSource/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/DataSources/DAN/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/DataSources/NI6259/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/DataSources/NI6368/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/DataSources/SDN/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/DataSources/UDP/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/DataSources/MDSWriter/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/DataSources/RealTimeThreadSynchronisation/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/DataSources/FileDataSource/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/GAMs/IOGAM/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/GAMs/BaseLib2GAM/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/GAMs/ConversionGAM/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/GAMs/FilterGAM/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/GAMs/StatisticsGAM/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/GAMs/WaveformGAM/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/GAMs/MathExpressionGAM/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/GAMs/ConstantGAM/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/Interfaces/BaseLib2Wrapper/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/Interfaces/SysLogger/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MARTe2_Components_DIR/Build/x86-linux/Components/Interfaces/EPICS/
+
+echo $LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+
+MARTeAPP=$MARTe2_DIR/Build/x86-linux/App/MARTeApp.ex
+
+#Start with cgdb or with strace
+if [ "$DEBUG" = "cgdb" ]
+then
+    if [ -z ${STATE+x} ]; then
+        gdb --args $MARTeAPP -l $LOADER -f $FILE -m $MESSAGE
+    else
+        gdb --args $MARTeAPP -l $LOADER -f $FILE -s $STATE
+    fi
+elif [ "$DEBUG" = "strace" ]
+then
+    if [ -z ${STATE+x} ]; then
+        strace -o/tmp/strace.err $MARTeAPP -l $LOADER -f $FILE -m $MESSAGE
+    else
+        strace -o/tmp/strace.err $MARTeAPP -l $LOADER -f $FILE -s $STATE
+    fi
+else
+    if [ -z ${STATE+x} ]; then
+        $MARTeAPP -l $LOADER -f $FILE -m $MESSAGE
+    else
+        $MARTeAPP -l $LOADER -f $FILE -s $STATE 
+    fi
+fi
