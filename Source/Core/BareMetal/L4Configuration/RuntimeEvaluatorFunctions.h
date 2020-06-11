0044fbe2e34725ab71f4f6d28d640ab7b6eff466
f-rfx <65179125+f-rfx@users.noreply.github.com>
2020-06-11 11:43:23 +0200

    Linting 23: fixed typos in justifications

--- Source/Core/BareMetal/L4Configuration/RuntimeEvaluatorFunctions.h
+++ Source/Core/BareMetal/L4Configuration/RuntimeEvaluatorFunctions.h
@@ -241,8 +241,7 @@ void RegisterFunction(const RuntimeEvaluatorFunctions &record);
  *          to be fed with the corresponding function via the
  *          RegisterFunction method.
  */
-/*lint -emacro( {446},  REGISTER_PCODE_FUNCTION ) Justification: Type2TypeDescriptor<>() is known to have no side-effects and can thus be used safely in ..._FunctionTypes[] array initialization.*/
-/*lint -emacro( {1502}, REGISTER_PCODE_FUNCTION ) Justification: the 'name ## subName ## RegisterClass' class intentionally has no data member. */
+/*lint -emacro( {446, 1502},  REGISTER_PCODE_FUNCTION ) Justification: Type2TypeDescriptor<>() is known to have no side-effects and can thus be used safely in ..._FunctionTypes[] array initialization.*/
 #define REGISTER_PCODE_FUNCTION(name,subName,nInputs,nOutputs,functionIn,...)\
     static TypeDescriptor name ## subName ## _FunctionTypes[] = {__VA_ARGS__}; \
     static const RuntimeEvaluatorFunctions name ## subName ## _RuntimeEvaluatorFunctions(#name,nInputs,nOutputs,name ## subName ## _FunctionTypes,&functionIn); \
