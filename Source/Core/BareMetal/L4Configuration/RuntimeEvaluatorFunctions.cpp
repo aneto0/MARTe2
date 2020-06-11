a1575c27925258d9f701e5ad0bb508034b19783e
f-rfx <65179125+f-rfx@users.noreply.github.com>
2020-06-11 11:57:00 +0200

    Linting 23: fixed typos in justifications

--- Source/Core/BareMetal/L4Configuration/RuntimeEvaluatorFunctions.cpp
+++ Source/Core/BareMetal/L4Configuration/RuntimeEvaluatorFunctions.cpp
@@ -195,10 +195,10 @@ REGISTER_PCODE_FUNCTION(WRITE,int8,1,0,Write<int8>     ,SignedInteger8Bit   ,Sig
  **********************************************************************************************************/
 
 #define REGISTER_CAST_FUNCTION(name,type1,type2,function)\
-    /*lint -e(446) Type2TypeDescriptor<>() has no side-effects. */ \
+    /*lint --emacro(446) Type2TypeDescriptor<>() has no side-effects. */ \
     static TypeDescriptor name ## type1 ## type2 ## _FunctionTypes[] = {Type2TypeDescriptor<type1>(), Type2TypeDescriptor<type2>()}; \
     static const RuntimeEvaluatorFunctions name ## type1 ## type2 ## _RuntimeEvaluatorFunctions(#name,1,1,name ## type1 ## type2 ## _FunctionTypes,&function<type1,type2>); \
-    /*lint -e{1502} class intentionally has no data member. */ \
+    /*lint --emacro{1502} class intentionally has no data member. */ \
     static class name ## type1 ## type2 ## RegisterClass { \
     public: name ## type1 ## type2 ## RegisterClass(){\
             RegisterFunction(name ## type1 ## type2 ## _RuntimeEvaluatorFunctions);\
