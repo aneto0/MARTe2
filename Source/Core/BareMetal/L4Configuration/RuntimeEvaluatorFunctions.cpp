abb10e448e97433dbd12c1d607654d09d10af2bc
f-rfx <65179125+f-rfx@users.noreply.github.com>
2020-06-11 10:36:23 +0200

    Trying (3)  the --emacro expression for justifications inside macros of RuntimeEvaluatorFunctions

--- Source/Core/BareMetal/L4Configuration/RuntimeEvaluatorFunctions.cpp
+++ Source/Core/BareMetal/L4Configuration/RuntimeEvaluatorFunctions.cpp
@@ -195,7 +195,7 @@ REGISTER_PCODE_FUNCTION(WRITE,int8,1,0,Write<int8>     ,SignedInteger8Bit   ,Sig
  **********************************************************************************************************/
 
 #define REGISTER_CAST_FUNCTION(name,type1,type2,function)\
-    /*lint -e{446} Justification: Type2TypeDescriptor<>() is know to have no side-effects and can thus be used safely in  ..._FunctionTypes[] array initialization.*/ \
+    /*lint -e(446) Justification: Type2TypeDescriptor<>() is know to have no side-effects and can thus be used safely in  ..._FunctionTypes[] array initialization.*/ \
     static TypeDescriptor name ## type1 ## type2 ## _FunctionTypes[] = {Type2TypeDescriptor<type1>(), Type2TypeDescriptor<type2>()}; \
     static const RuntimeEvaluatorFunctions name ## type1 ## type2 ## _RuntimeEvaluatorFunctions(#name,1,1,name ## type1 ## type2 ## _FunctionTypes,&function<type1,type2>); \
     /*lint -e{1502} Justification: the following class intentionally has no data member. */ \
