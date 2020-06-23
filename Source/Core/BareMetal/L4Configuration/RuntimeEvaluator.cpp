07f1f2e7fd5666cee551013f8180b896948fa5a1
f-rfx <65179125+f-rfx@users.noreply.github.com>
2020-06-23 14:58:17 +0200

    moved one comment

--- Source/Core/BareMetal/L4Configuration/RuntimeEvaluator.cpp
+++ Source/Core/BareMetal/L4Configuration/RuntimeEvaluator.cpp
@@ -163,8 +163,7 @@ ErrorManagement::ErrorType RuntimeEvaluator::AddVariable2DB(const CCString &name
     ret = FindVariableinDB(name,variableToSearch,db);
     
     /*lint -e(909) . Justification: the conversion of unsupportedFeature to bool is handled. */
-    if (ret.unsupportedFeature){
-        // if it is already there we do not need to add
+    if (ret.unsupportedFeature) { // if it is already there we do not need to add
         /*lint --e{429} . Justification: the allocated memory is freed by the class destructor. */
         VariableInformation *variableInfo = new VariableInformation;
         variableInfo->name = name;
