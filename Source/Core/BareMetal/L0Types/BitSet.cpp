0f2506e6005b81bde63cb05491dce5c1ce4dec68
Martino Ferrari <manda.mgf@gmail.com>
2021-02-10 14:23:00 +0100

    improved formatting of MACRO (773 lint)

--- Source/Core/BareMetal/L0Types/BitSet.cpp
+++ Source/Core/BareMetal/L0Types/BitSet.cpp
@@ -1,8 +1,8 @@
 
 #include "BitSet.h"
 
-#define MAX(a, b) (a > b) ? a : b
-#define MIN(a, b) (a < b) ? a : b
+#define MAX(a, b) ((a > b) ? a : b)
+#define MIN(a, b) ((a < b) ? a : b)
 
 namespace MARTe{
 
