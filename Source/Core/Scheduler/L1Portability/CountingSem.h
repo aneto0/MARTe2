e8c1a5aae2c4460d55281507abc9d0386345c0ce
Martino Ferrari <martinogiordano.ferrari@iter.org>
2021-04-12 12:31:26 +0200

    added doxygen documentation

--- Source/Core/Scheduler/L1Portability/CountingSem.h
+++ Source/Core/Scheduler/L1Portability/CountingSem.h
@@ -39,29 +39,86 @@
 
 namespace MARTe{
 
+/**
+ * @brief Counting Semaphore 
+ *
+ * @details The CountingSem extends the EventSem class by implementing a 
+ * simple counter logic. In order to release the barrier the method 
+ * `WaitForAll` must be called at least N times, where N is the number of 
+ * actors defined in the `Create` method.
+ */
 class CountingSem: public EventSem {
 public:
+    /**
+     * @brief Initialize parent class and private fields.
+     */
     CountingSem();
-
+    
+    /**
+     * @brief TODO
+     */
     virtual ~CountingSem();
 
+    /**
+     * @brief Create the semaphore and set the number of actors to wait.
+     * @return true if the operating system call returns without errors.  
+     */
     bool Create(uint32 numberOfActorsIn);
 
-
+    /**
+     * @brief Wait for a `Post` event when all actors have checked in or
+     * when the timeout is over.
+     *
+     * @param[in] timeout the maximum time the barrier will last
+     * @return ErrorManagement::NoError if the operating system call returns 
+     * with no errors or Timeout if the time waiting in the barrier (from when 
+     * the function was called) was greater than the specified timeout.
+     * @pre the semaphore was successfully created.
+     */
     ErrorManagement::ErrorType WaitForAll(const TimeoutType &timeout);
 
 
+    /**
+     * @brief Reset the semaphore, raises the barrier and resets the counter.
+     * The Reset is only possible when the `counter` is equal or greater then
+     * the defined number of actors.
+     * @return true if the operating system call returns without errors.
+     * @pre the semaphore was successfully created.
+     */
     bool Reset();
-
+    
+    /**
+     * @brief Force the reset of the semaphore even if the condition on the 
+     * counter is not met.
+     * @return true if the operating system call returns without errors.
+     * @pre the semaphore was successfully created.
+     */ 
     bool ForceReset();
-
+    
+    /**
+     * @brief Force the post action even if the conditions on the counter and
+     * on the timeout are not met.
+     * @return true if the operating systemc call returns without errors.
+     * @pre the semaphore was successfully created.
+     */
     bool ForcePass();
 
 private:
+    /**
+     * Number of Actors to wait and synchronize.
+     */
     uint32 numberOfActors;
 
+    /**
+     * Counter used to count the number of actros that have already
+     * checked in.
+     */
     uint32 counter;
 
+    /**
+     * Simple semaphore used to protect the access to the counter 
+     * field.
+     */
     FastPollingMutexSem sem;
 };
 
