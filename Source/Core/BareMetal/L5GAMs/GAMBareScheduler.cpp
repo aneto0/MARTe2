c74662490a2b52e7efbd02e221bdf22e53e5b050
Giuseppe Avon <giuseppe.avon@phd.unict.it>
2021-11-08 12:21:02 +0100

    Fixed cycle calculation

--- Source/Core/BareMetal/L5GAMs/GAMBareScheduler.cpp
+++ Source/Core/BareMetal/L5GAMs/GAMBareScheduler.cpp
@@ -69,21 +69,22 @@ ErrorManagement::ErrorType GAMBareScheduler::StartNextStateExecution() {
 
     while(isAlive) { 
         Cycle(0u); 
+
+        uint32 absTime = 0u;
+        if (lastCycleTimeStamp != 0u) {
+            uint64 tmp = (HighResolutionTimer::Counter() - lastCycleTimeStamp);
+            float64 ticksToTime = (static_cast<float64>(tmp) * clockPeriod) * 1e6;
+            absTime = static_cast<uint32>(ticksToTime);  //us
+        }
+        uint32 sizeToCopy = static_cast<uint32>(sizeof(uint32));
+        if (!MemoryOperationsHelper::Copy(nextState->threads[0].cycleTime, &absTime, sizeToCopy)) {
+            REPORT_ERROR(ErrorManagement::FatalError, "Could not copy cycle time information.");
+        }
+        lastCycleTimeStamp = HighResolutionTimer::Counter();
+
         if (maxCycles != 0u) {
             isAlive = (nCycle < maxCycles);
             nCycle++;
-
-            uint32 absTime = 0u;
-            if (lastCycleTimeStamp != 0u) {
-                uint64 tmp = (HighResolutionTimer::Counter() - lastCycleTimeStamp);
-                float64 ticksToTime = (static_cast<float64>(tmp) * clockPeriod) * 1e6;
-                absTime = static_cast<uint32>(ticksToTime);  //us
-            }
-            uint32 sizeToCopy = static_cast<uint32>(sizeof(uint32));
-            if (!MemoryOperationsHelper::Copy(nextState->threads[0].cycleTime, &absTime, sizeToCopy)) {
-                REPORT_ERROR(ErrorManagement::FatalError, "Could not copy cycle time information.");
-            }
-            lastCycleTimeStamp = HighResolutionTimer::Counter();
         }
     }
     
