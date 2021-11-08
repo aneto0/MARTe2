80579c8907ec2139b55b9b055be2c1a3a546da46
Giuseppe Avon <giuseppe.avon@phd.unict.it>
2021-11-08 11:56:06 +0100

    Tuning prioritites

--- Source/Core/Scheduler/L1Portability/Environment/FreeRTOS/Threads.cpp
+++ Source/Core/Scheduler/L1Portability/Environment/FreeRTOS/Threads.cpp
@@ -320,7 +320,7 @@ ThreadIdentifier BeginThread(const ThreadFunctionType function,
                 (name==NULL)?("Unknown"):(name), 
                 (stacksize < configMINIMAL_STACK_SIZE)?(configMINIMAL_STACK_SIZE):(stacksize), 
                 static_cast<void*>(tParams),
-                (configMAX_PRIORITIES - 1) /*| portPRIVILEGE_BIT*/,
+                tskIDLE_PRIORITY /*| portPRIVILEGE_BIT*/,
                 &threadId);
 
             bool ok = (ret == pdPASS);
