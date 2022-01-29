ba36b737535dd5447951c38b93d60b48ddf95e12
Giuseppe Avon <giuseppe.avon@phd.unict.it>
2022-01-29 13:44:08 +0100

    FreeRTOS Bootstrap with compile-time configurable ReadParameters

--- Source/Core/BareMetal/L6App/Environment/FreeRTOS/Bootstrap.cpp
+++ Source/Core/BareMetal/L6App/Environment/FreeRTOS/Bootstrap.cpp
@@ -80,15 +80,34 @@ void Bootstrap::Printf(const char8 * const msg) {
 }
 
 ErrorManagement::ErrorType Bootstrap::ReadParameters(int32 argc, char8 **argv, StructuredDataI &loaderParameters) {
-    //TODO finishe me
+    //TODO Check me
     ErrorManagement::ErrorType ret;
-    ret.parametersError = !loaderParameters.Write("Loader", "RealTimeLoader");
+
+    //If we have the compilation variable MARTe2_READPARAMETERS_LOADER defined, then we can use that
+    //otherwise we use our default one, the RealTimeLoader
+    #ifdef (MARTe2_READPARAMETERS_LOADER)
+        ret.parametersError = !loaderParameters.Write("Loader", MARTe2_READPARAMETERS_LOADER);
+    #else
+        ret.parametersError = !loaderParameters.Write("Loader", "RealTimeLoader");
+    #endif
+
     if (ret) {
-        ret.parametersError = !loaderParameters.Write("FirstState", "Idle");
-        //ret.parametersError = !loaderParameters.Write("MessageDestination", "StateMachine");
-        //if (ret) {
-        //    ret.parametersError = !loaderParameters.Write("MessageFunction", "START");
-        //}
+        //If we have the compilation variable MARTe2_READPARAMETERS_FIRSTSTATE defined, then we can use that
+        //otherwise we use our default one, Idle
+        #ifdef (MARTe2_READPARAMETERS_FIRSTSTATE)
+            ret.parametersError = !loaderParameters.Write("FirstState", MARTe2_READPARAMETERS_FIRSTSTATE);
+        #else
+            ret.parametersError = !loaderParameters.Write("FirstState", "Idle");
+        #endif
+
+        //If we have both MARTe2_READPARAMETERS_FIRSTMESSAGEDESTINATION MARTe2_READPARAMETERS_FIRSTMESSAGEFUNCTION
+        //we can send a first message to the state machine startup. Otherwise, nothing will be sent
+        #if defined (MARTe2_READPARAMETERS_FIRSTMESSAGEDESTINATION) && defined (MARTe2_READPARAMETERS_FIRSTMESSAGEFUNCTION)
+            ret.parametersError = !loaderParameters.Write("MessageDestination", "StateMachine");
+            if (ret) {
+                ret.parametersError = !loaderParameters.Write("MessageFunction", "START");
+            }
+        #endif
     }
     if (ret) {
         ret.parametersError = !loaderParameters.Write("Parser", "cdb");
