d72e1dc139a0219d7016ebe3ee1034d9b6b16748
André Neto <andre.neto@f4e.europa.eu>
2022-03-17 16:09:15 +0000

    Update CompilerTypes.h

--- Source/Core/BareMetal/L0Types/Architecture/armv8_gcc/CompilerTypes.h
+++ Source/Core/BareMetal/L0Types/Architecture/armv8_gcc/CompilerTypes.h
@@ -1,8 +1,8 @@
 /**
  * @file CompilerTypes.h
  * @brief Header file for CompilerTypes
- * @date 17/06/2015
- * @author Giuseppe Ferr�
+ * @date 17/06/2021
+ * @author Giuseppe Avon
  *
  * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
  * the Development of Fusion Energy ('Fusion for Energy').
