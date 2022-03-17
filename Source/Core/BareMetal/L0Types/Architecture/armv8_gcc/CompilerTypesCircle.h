4206dc2697cf9180b1f95796e29bf9e8a8f82e99
André Neto <andre.neto@f4e.europa.eu>
2022-03-17 16:09:32 +0000

    Update CompilerTypesCircle.h

--- Source/Core/BareMetal/L0Types/Architecture/armv8_gcc/CompilerTypesCircle.h
+++ Source/Core/BareMetal/L0Types/Architecture/armv8_gcc/CompilerTypesCircle.h
@@ -2,7 +2,7 @@
  * @file CompilerTypesCircle.h
  * @brief Header file for CompilerTypesCircle
  * @date 17/06/2015
- * @author Giuseppe Ferr�
+ * @author Giuseppe Avon
  *
  * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
  * the Development of Fusion Energy ('Fusion for Energy').
