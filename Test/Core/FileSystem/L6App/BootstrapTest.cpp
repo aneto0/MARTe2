bce6a0adb3beb72f1618951a15b7f437dad873fd
André Neto <Andre.Neto@f4e.europa.eu>
2022-10-07 17:04:29 +0200

    Close bootstrap configuration stream

--- Test/Core/FileSystem/L6App/BootstrapTest.cpp
+++ Test/Core/FileSystem/L6App/BootstrapTest.cpp
@@ -179,7 +179,7 @@ bool BootstrapTest::TestGetConfigurationStream_False() {
     const char8 *argv[] = { "Test", "-l", "LOADER", "-f", filename};
 
     Bootstrap bp;
-    //bp.CloseConfigurationStream(); TODO uncomment when featured is merged into develop
+    bp.CloseConfigurationStream(); 
     ConfigurationDatabase loaderParameters;
     bool ok = bp.ReadParameters(argc, const_cast<char8 **>(argv), loaderParameters);
     if (ok) {
