06c93133af59b1b37057520df09e68d4a4ea77f8
André Neto <Andre.Neto@f4e.europa.eu>
2024-04-03 22:55:50 +0200

    Correct author name

--- Source/Core/BareMetal/L1Portability/Environment/Windows/HighResolutionTimerCalibrator.h
+++ Source/Core/BareMetal/L1Portability/Environment/Windows/HighResolutionTimerCalibrator.h
@@ -2,7 +2,7 @@
  * @file HighResolutionTimerCalibrator.h
  * @brief Header file for class HighResolutionTimerCalibrator
  * @date 17/06/2015
- * @author Giuseppe Ferr�
+ * @author Giuseppe Ferro
  *
  * @copyright Copyright 2015 F4E | European Joint Undertaking for ITER and
  * the Development of Fusion Energy ('Fusion for Energy').
