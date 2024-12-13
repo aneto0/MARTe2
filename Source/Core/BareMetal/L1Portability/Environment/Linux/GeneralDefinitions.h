9915670751884ba000aa6ffa8b7a0b904d0faf0f
nferron <26-nferron@users.noreply.vcis-gitlab.f4e.europa.eu>
2024-12-13 15:52:37 +0000

    change order of OS library extension to prevent GAM not found warning

--- Source/Core/BareMetal/L1Portability/Environment/Linux/GeneralDefinitions.h
+++ Source/Core/BareMetal/L1Portability/Environment/Linux/GeneralDefinitions.h
@@ -76,7 +76,7 @@ typedef size_t osulong;
 /**
  * Know shared library extensions
  */
-static const char8 * const operatingSystemDLLExtensions[] = { ".gam", ".drv", ".so", static_cast<char8 *>(NULL) };
+static const char8 * const operatingSystemDLLExtensions[] = {".so", ".gam", ".drv", static_cast<char8 *>(NULL) };
 
 struct MutexSemProperties;
 #define MutexSemHandle MutexSemProperties*
